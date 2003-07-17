da2dbb3252255892e8404dca8411aedc23ed964c
Tom G. Christensen <tgc@jupiterrise.com>
2003-07-17 07:40:35 +0000

    GNU Coreutils containing fileutils, sh-utils & textutils

---
+++ coreutils/build.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=coreutils    
+version=5.0
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${HOME}/buildpkg/scripts/buildpkg.functions
+
+# Fill in pkginfo values if necessary
+# using pkgname,name,pkgcat,pkgvendor & pkgdesc
+name="Gnu Coreutils"
+pkgvendor="http://www.gnu.org"
+pkgdesc="Collection of basic file, shell and text manipulation utilities"
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
