e82aaf705cd08d524c0b315695bc85fffaf9a23a
Tom G. Christensen <tgc@jupiterrise.com>
2004-01-29 15:45:06 +0000

    Remove info/dir file

--- diffutils/build.sh
+++ diffutils/build.sh
@@ -9,7 +9,7 @@
 # Check the following 4 variables before running the script
 topdir=diffutils
 version=2.8.1
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -47,6 +47,7 @@ reg install
 install()
 {
     generic_install DESTDIR
+    $RM -f $stagedir$prefix/info/dir
 }
 
 reg pack
