9e98396794e6f701071379f07c04c7dbb82f6119
Tom G. Christensen <tgc@jupiterrise.com>
2004-04-15 08:03:38 +0000

    Update to gcc 3.3.3 With the new pkgdef file build.sh has been cleaned of all the hoop jumping needed to build multiple packages before.

--- gcc/build.sh
+++ gcc/build.sh
@@ -8,8 +8,8 @@
 #
 # Check the following 4 variables before running the script
 topdir=gcc
-version=3.3.2
-pkgver=2
+version=3.3.3
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 ## If there are no patches, simply comment this
 #patch[0]=
@@ -17,15 +17,9 @@ source[0]=$topdir-$version.tar.bz2
 # Source function library
 . ${HOME}/buildpkg/scripts/buildpkg.functions
 
-# Define abbreviated version number
-abbrev_ver=$(echo $version|sed -e 's/\.//g') #331
-
 # Fill in pkginfo values if necessary
 # using pkgname,name,pkgcat,pkgvendor & pkgdesc
-pkgname="$pkgprefix""gcc""$abbrev_ver"
-name="GCC - GNU Compiler Collection"
-pkgvendor="http://gcc.gnu.org"
-pkgdesc="GNU Compiler Collection (C, C++)"
+# see $metadir/pkgdef
 
 # depend is created by build.sh so make sure
 # it's removed by distclean
@@ -33,25 +27,12 @@ META_CLEAN="$META_CLEAN depend"
 
 # We're going to build more than one package from this source
 # define helpervars to do that
-libgcc_stage=$BUILDPKG_BASE/$topdir/stage.libgcc
-gcc_dir=$prefix/gcc-$version
-libgcc_pkgname="$pkgprefix""libgccso1"
-libgcc_name="libgcc - GCC runtime support"
-libgcc_pkgcat="library"
-libgcc_pkgvendor="http://gcc.gnu.org"
-libgcc_pkgdesc="Runtime support for programs built with gcc 3.x"
-libgcc_pkgver="2"
 
-libstdc_stage=$BUILDPKG_BASE/$topdir/stage.libstdc
-libstdc_pkgname="$pkgprefix""libstdcso5"
-libstdc_name="libstdc++ - GCC runtime support"
-libstdc_pkgcat="library"
-libstdc_pkgvendor="http://gcc.gnu.org"
-libstdc_pkgdesc="Runtime support for c++ programs built with gcc $version"
-libstdc_pkgver="2"
+# Define abbreviated version number (for pkgdef)
+abbrev_ver=$(echo $version|sed -e 's/\.//g') #331
 
-MV=/usr/bin/mv
-CP=/usr/bin/cp
+# gcc base dir (for pkgdef)
+gccdir=gcc-$version
 
 objdir=$srcdir/objdir
 
@@ -72,7 +53,7 @@ build()
 {
     $MKDIR "$objdir"
     setdir "$objdir"
-    $srcdir/$topsrcdir/configure --prefix=/usr/local/gcc-$version --disable-nls --with-as=/usr/ccs/bin/as --with-ld=/usr/ccs/bin/ld --with-system-zlib --enable-languages=c,c++ --with-cpu=ultrasparc
+    $srcdir/$topsrcdir/configure --prefix=/usr/local/gcc-$version --disable-nls --with-as=/usr/ccs/bin/as --with-ld=/usr/ccs/bin/ld --with-system-zlib --enable-languages=c,c++,f77,java --with-cpu=ultrasparc
     $MAKE_PROG
 }
 
@@ -90,68 +71,28 @@ pack()
 
     # We want to create gcc, libstdc++ and libgcc packages
     # Copy files for libgcc package
-    $MKDIR -p $libgcc_stage$prefix/lib
-    $MKDIR -p $libgcc_stage$prefix/lib/sparcv9
-    $CP $stagedir$gcc_dir/lib/libgcc_s* $libgcc_stage$prefix/lib
-    $CP $stagedir$gcc_dir/lib/sparcv9/libgcc_s* $libgcc_stage$prefix/lib/sparcv9
+    $MKDIR -p $stagedir$prefix/lib
+    $MKDIR -p $stagedir$prefix/lib/sparcv9
+    $CP $stagedir$prefix/$gccdir/lib/libgcc_s* $stagedir$prefix/lib
+    $CP $stagedir$prefix/$gccdir/lib/sparcv9/libgcc_s* $stagedir$prefix/lib/sparcv9
 
     # Copy files for libstdc++ package
-    $MKDIR -p $libstdc_stage$prefix/lib
-    $MKDIR -p $libstdc_stage$prefix/lib/sparcv9
-    $CP $stagedir$gcc_dir/lib/libstdc++.so* $libstdc_stage$prefix/lib
-    $CP $stagedir$gcc_dir/lib/sparcv9/libstdc++.so* $libstdc_stage$prefix/lib/sparcv9
-    #rm -f $libstdc_stage$prefix/lib/libstdc++.so.5
-    #rm -f $libstdc_stage$prefix/lib/sparcv9/libstdc++.so.5
-    rm -f $libstdc_stage$prefix/lib/libstdc++.so
-    rm -f $libstdc_stage$prefix/lib/sparcv9/libstdc++.so
+    $MKDIR -p $stagedir$prefix/lib
+    $MKDIR -p $stagedir$prefix/lib/sparcv9
+    $CP $stagedir$prefix/$gccdir/lib/libstdc++.so* $stagedir$prefix/lib
+    $CP $stagedir$prefix/$gccdir/lib/sparcv9/libstdc++.so* $stagedir$prefix/lib/sparcv9
+    rm -f $stagedir$prefix/lib/libstdc++.so
+    rm -f stagedirlibstdc_stage$prefix/lib/sparcv9/libstdc++.so
     
-    # now create gcc package
+    # now create packages according to pkgdef
     generic_pack
-
-    # Prepare for libgcc package
-    $MV $stagedir $stagedir.1
-    $MV $libgcc_stage $stagedir
-    pkgname=$libgcc_pkgname
-    name=$libgcc_name
-    pkgcat=$libgcc_pkgcat
-    pkgvendor=$libgcc_pkgvendor
-    pkgdesc=$libgcc_pkgdesc
-    pkgver=$libgcc_pkgver
-
-    distfile=libgcc-$version-$pkgver.sb-$os-$cpu-$pkgdirdesig
-    generic_pack # don't embed any pre/post scripts
-
-    # Prepare for libstdc++ package
-    $MV $stagedir $libgcc_stage
-    $MV $libstdc_stage $stagedir
-    pkgname=$libstdc_pkgname
-    name=$libstdc_name
-    pkgcat=$libstdc_pkgcat
-    pkgvendor=$libstdc_pkgvendor
-    pkgdesc=$libstdc_pkgdesc
-    pkgver=$libstdc_pkgver
-
-    distfile=libstdc++-$version-$pkgver.sb-$os-$cpu-$pkgdirdesig
-    usedepend=0
-    generic_pack
-    
-    # clean up $stagedir
-    # We will need to do build.sh install to use it again since we've moved files around
-    # so we might as well nuke it now
-    $MV $stagedir.1 $stagedir # move gcc stagedir into the current stagedir
-    $MV $libgcc_stage $stagedir # move libgcc stagedir into the current stagedir
-    setdir source
-    clean stage  # Nuke all of it :)
 }
 
 reg distclean
 distclean()
 {
+    clean meta
     clean distclean
-    pkgname=$libgcc_pkgname
-    clean meta
-    pkgname=$libstdc_pkgname
-    clean meta
     $RM -rf $objdir
 }
 
