fb17de193d48985be4509ff6bb65d7ba181f6469
Tom G. Christensen <tgc@jupiterrise.com>
2004-06-14 12:32:20 +0000

    New upstream version fixes another few security holes

--- cvs/build.sh
+++ cvs/build.sh
@@ -8,7 +8,7 @@
 #
 # Check the following 4 variables before running the script
 topdir=cvs
-version=1.11.16
+version=1.11.17
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
