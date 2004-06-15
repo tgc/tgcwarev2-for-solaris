9c53fde45837768aa6cc9ad9176934e2be15b4ae
Tom G. Christensen <tgc@jupiterrise.com>
2004-06-15 11:15:47 +0000

    New upstream release Converted to new pkgdef style

--- coreutils/build.sh
+++ coreutils/build.sh
@@ -8,20 +8,14 @@
 #
 # Check the following 4 variables before running the script
 topdir=coreutils    
-version=5.0
-pkgver=2
+version=5.2.1
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
 # Source function library
 . ${HOME}/buildpkg/scripts/buildpkg.functions
-
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="GNU Coreutils"
-pkgvendor="http://www.gnu.org"
-pkgdesc="Collection of basic file, shell and text manipulation utilities"
 
 # Define script functions and register them
 METHODS=""
@@ -38,6 +32,7 @@ prep()
 reg build
 build()
 {
+    export LDFLAGS="-R/usr/local/lib"
     generic_build
 }
 
@@ -45,7 +40,10 @@ reg install
 install()
 {
     generic_install DESTDIR
-    $RM -f $stagedir$prefix/info/dir
+    $RM -f ${stagedir}${prefix}/${_infodir}/dir
+    # We're not installing su so nuke the manpage...
+    $RM -f ${stagedir}${prefix}/${_mandir}/man1/su.1
+    doc NEWS THANKS THANKS-to-translators TODO README AUTHORS ChangeLog
 }
 
 reg pack
