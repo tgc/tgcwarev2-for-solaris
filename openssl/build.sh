1471142289294952b919e94182a46ffe892a51d9
Tom G. Christensen <tgc@jupiterrise.com>
2004-10-07 12:00:25 +0000

    First 0.9.7d package in CVS. - Move to pkgdef - Avoid libgcc_s.so.1 depend

--- openssl/build.sh
+++ openssl/build.sh
@@ -8,40 +8,21 @@
 #
 # Check the following 4 variables before running the script
 topdir=openssl
-version=0.9.7c
-pkgver=5
+version=0.9.7d
+pkgver=4
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=openssl-0.9.7c-shlib.patch
 patch[1]=openssl-0.9.7c-Configure.patch
-patch[2]=openssl-0.9.7c-doc.patch
-patch[3]=openssl-0.9.7c-can-2004-0079.patch
-patch[4]=openssl-0.9.7c-can-2004-0112.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # shared library binary compatibility is not guaranteed
 # Play it safe and up the soversion with each release
-sover=3 # c = 3
+sover=4 # d = 4
 abbrev_ver=$(echo $version|$SED -e 's/\.//g')
 baseversion=$(echo $version|$SED -e 's/[a-zA-Z]//g')
-
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-pkgname="$pkgprefix""ossl""$abbrev_ver"
-name="OpenSSL - Secure Socket Layer"
-pkgcat="application"
-pkgvendor="http://www.openssl.org"
-pkgdesc="Toolkit implementing SSL v2/v3 and TLS v1"
-
-pkgname_lib="$pkgprefix""ossl""$abbrev_ver""lib"
-name_lib="OpenSSL - Secure Socket Layer"
-pkgcat_lib="library"
-pkgvendor_lib="http://www.openssl.org"
-pkgdesc_lib="Toolkit implementing SSL v2/v3 and TLS v1"
-
-lib_stage=$BUILDPKG_BASE/$topdir/stage.lib
 
 # Define script functions and register them
 METHODS=""
@@ -72,8 +53,8 @@ build()
     $SED -e "s;${major};SHLIB_MAJOR=${baseversion};g" \
 	-e "s;${minor};SHLIB_MINOR=${sover};g" Makefile.ssl > Makefile.new
     $MV Makefile.new Makefile.ssl
-    $MAKE_PROG LIBSSL="-Wl,-R,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-R,$prefix/lib -L.. -lcrypto" all build-shared
-    $MAKE_PROG LIBSSL="-Wl,-R,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-R,$prefix/lib -L.. -lcrypto" all link-shared do_solaris-shared
+    $MAKE_PROG CC="gcc -static-libgcc" LIBSSL="-Wl,-R,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-R,$prefix/lib -L.. -lcrypto" all build-shared
+    $MAKE_PROG CC="gcc -static-libgcc" LIBSSL="-Wl,-R,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-R,$prefix/lib -L.. -lcrypto" all link-shared do_solaris-shared
 }
 
 reg install
@@ -81,7 +62,7 @@ install()
 {
     setdir source
     clean stage
-    $MAKE_PROG INSTALL_PREFIX=$stagedir LIBSSL="-Wl,-R,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-R,$prefix/lib -L.. -lcrypto" install
+    $MAKE_PROG CC="gcc -static-libgcc" INSTALL_PREFIX=$stagedir LIBSSL="-Wl,-R,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-R,$prefix/lib -L.. -lcrypto" install
     setdir $stagedir$prefix/lib
     chmod a+x pkgconfig
     rmdir $stagedir$prefix/ssl/lib
@@ -103,41 +84,20 @@ install()
 	cd ..
     done
     # A few stupid manpages left that pkgproto can't deal with
-    setdir $stagedir$prefix/man/man3
-    mv "EVP_MD_CTX_copy_ex EVP_MD_CTX_copy.3ssl" "EVP_MD_CTX_copy_ex_EVP_MD_CTX_copy.3ssl"
-    mv "UI_construct_prompt UI_add_user_data.3ssl" "UI_construct_prompt_UI_add_user_data.3ssl"
     setdir $stagedir$prefix/man/man7
     mv "Modes of DES.7ssl" "Modes_of_DES.7ssl"
+    # Make .sos writable
+    chmod 755 ${stagedir}${prefix}/${_libdir}/*.so.*
+    custom_install=1
+    generic_install
 }
 
 reg pack
 pack()
 {
-    # Split up the stagedir
-    # The bare .so and .a used for development should only be available
-    # if the matching headers etc. is installed so they're not put in the lib package
-    mkdir -p $lib_stage$prefix/lib
-    $MV $stagedir$prefix/lib/*.so.* $lib_stage$prefix/lib
-
-    # Create runtime package
-    echo "P $pkgname_lib	$name_lib" > $metadir/depend
+    # Create depend file for openssl
+    echo "P ${pkgprefix}ossl${abbrev_ver}lib	OpenSSL $version shared libraries" > $metadir/depend.openssl
     generic_pack
-
-    usedepend=0
-    $MV $stagedir $stagedir.1
-    $MV $lib_stage $stagedir
-    pkgname=$pkgname_lib
-    name=$name_lib
-    pkgcat=$pkgcat_lib
-    pkgvendor=$pkgvendor_lib
-    pkgdesc=$pkgdesc_lib
-
-    distfile=$topdir-lib-$version-$pkgver.sb-$os-$cpu-$pkgdirdesig
-    generic_pack
-
-    $MV $stagedir.1 $stagedir
-    setdir source
-    clean stage
 }
 
 reg distclean
@@ -145,11 +105,9 @@ distclean()
 {
     # depend is created by build.sh so make sure
     # it's removed by distclean
-    META_CLEAN="$META_CLEAN depend"
+    META_CLEAN="$META_CLEAN depend.openssl"
 
     clean distclean
-    pkgname=$pkgname_lib
-    clean meta
 }
 
 ###################################################
