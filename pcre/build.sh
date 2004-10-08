337906edb7b8fd89157a3e1064900f430e0c12a6
Tom G. Christensen <tgc@jupiterrise.com>
2004-10-08 09:36:06 +0000

    New upstream version 4.5 "Proper" fix for the linking problems when installing to DESTDIR Use pkgdef

--- pcre/build.sh
+++ pcre/build.sh
@@ -8,21 +8,14 @@
 #
 # Check the following 4 variables before running the script
 topdir=pcre
-version=4.4
-pkgver=2
+version=4.5
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
-
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="PCRE"
-pkgcat="library"
-pkgvendor="http://www.pcre.org"
-pkgdesc="Perl Compatible Regular Expressions"
 
 # Define script functions and register them
 METHODS=""
@@ -39,15 +32,22 @@ prep()
 reg build
 build()
 {
+    configure_args='--prefix=$prefix --enable-utf8'
+    generic_build
     setdir source
-    ./configure --prefix=$prefix --disable-static --enable-utf8
-    $MAKE_PROG
+    $MAKE_PROG runtest
+    # Workaround libtool problems...
+    setdir source
+    ${SED} -e "s# libpcre.la# -L$srcdir/$topsrcdir/.libs libpcre.la#g" libpcreposix.la > libpcreposix.la.new
+    ${MV} libpcreposix.la.new libpcreposix.la
 }
 
 reg install
 install()
 {
     generic_install DESTDIR
+    doc NEWS AUTHORS ChangeLog LICENCE
+    ${RM} -f ${stagedir}${prefix}/${_libdir}/*.la
 }
 
 reg pack
