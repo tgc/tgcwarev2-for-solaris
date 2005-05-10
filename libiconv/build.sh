1ea610548ba19e36b6476a19fe7dd94b2d09fdbb
Tom G. Christensen <tgc@jupiterrise.com>
2005-05-10 13:13:56 +0000

    GNU iconv for Solaris 8

---
+++ libiconv/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libiconv
+version=1.9.2
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export LDFLAGS="-L/usr/sbware/lib -R/usr/sbware/lib"
+prefix=/usr/sbware
+topinstalldir=$prefix
+configure_args="--prefix=$prefix --mandir=$prefix/${_mandir} --enable-extra-encodings"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc NOTES ChangeLog DESIGN NEWS
+    ${RM} -f ${stagedir}${prefix}/${_libdir}/charset.alias
+
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
