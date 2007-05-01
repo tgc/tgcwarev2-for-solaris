293fa7dbd26eda312069df71f0f4eca8eba0d011
Tom G. Christensen <tgc@jupiterrise.com>
2007-05-01 09:41:31 +0000

    Update to 2.3 Newstyle pkg

--- bison/build.sh
+++ bison/build.sh
@@ -3,12 +3,13 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=bison
-version=1.875
+version=2.3
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -16,19 +17,6 @@ source[0]=$topdir-$version.tar.bz2
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
-
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="yacc replacement"
-pkgcat="application"
-pkgvendor="http://www.gnu.org"
-pkgdesc="General purpose parser generator"
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
 
 reg prep
 prep()
@@ -46,8 +34,6 @@ reg install
 install()
 {
     generic_install DESTDIR
-    echo $RM $stagedir$prefix/info/dir
-    $RM $stagedir$prefix/info/dir
 }
 
 reg pack
@@ -65,42 +51,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
