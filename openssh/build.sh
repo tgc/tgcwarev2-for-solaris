c9a7103f47826c5992fb87cf48cd7e456fec8749
Tom G. Christensen <tgc@jupiterrise.com>
2007-10-25 08:38:42 +0000

    Fix init script and rebuild.

--- openssh/build.sh
+++ openssh/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=openssh
 version=4.7p1
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
