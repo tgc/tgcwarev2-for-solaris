3657a85e13e4c655660b6c35506de4334cba6500
Tom G. Christensen <tgc@jupiterrise.com>
2008-04-20 09:51:49 +0000

    Update to 4.0.3. Update to newstyle buildpkg.

--- screen/build.sh
+++ screen/build.sh
@@ -3,32 +3,22 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=screen
-version=4.0.2
-pkgver=2
+version=4.0.3
+pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=screen-4.0.2-makefile-madness.diff
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="Screen"
-pkgcat="application"
-pkgvendor="http://www.guckes.net/screen/"
-pkgdesc="A fullscreen window manager for consoles"
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
 
 reg prep
 prep()
@@ -47,9 +37,9 @@ install()
 {
     generic_install DESTDIR
     setdir source
-    $CP terminfo/screeninfo.src $stagedir$prefix/share/$topdir
-    $MKDIR -p $stagedir$prefix/etc
-    $CP etc/etcscreenrc $stagedir$prefix/etc/screenrc
+    ${__cp} terminfo/screeninfo.src $stagedir$prefix/share/$topdir
+    ${__mkdir} -p $stagedir$prefix/etc
+    ${__cp} etc/etcscreenrc $stagedir$prefix/etc/screenrc
     # Add two useful hacks to screenrc
     cat << EOF >> $stagedir$prefix/etc/screenrc
 # special xterm hardstatus: use the window title.
@@ -57,7 +47,7 @@ termcapinfo xterm 'hs:ts=\E]2;:fs=\007:ds=\E]2;screen\007'
 
 hardstatus string "[screen %n%?: %t%?] %h"
 EOF
-    $RM -f $stagedir$prefix/info/dir
+    doc NEWS README doc/README.DOTSCREEN doc/FAQ
 }
 
 reg pack
@@ -75,42 +65,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
