0b837cc403a10200dc18d29f589f32338172461a
Tom G. Christensen <tgc@jupiterrise.com>
2008-04-25 19:19:39 +0000

    Update to 1.4.11

--- m4/build.sh
+++ m4/build.sh
@@ -3,33 +3,24 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=m4
-version=1.4.1
-pkgver=2
-source[0]=$topdir-$version.tar.gz
+version=1.4.11
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-patch[0]=m4-1.4.1-sec.patch
-patch[1]=m4-1.4.1-configure.patch
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="GNU m4 - Macro processor"
-pkgcat="application"
-pkgvendor="http://www.gnu.org"
-pkgdesc="An implementation of the traditional UNIX macro processor"
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 
 reg prep
 prep()
@@ -40,22 +31,25 @@ prep()
 reg build
 build()
 {
-    setdir source
-    autoreconf -f
     generic_build
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg install
 install()
 {
-    setdir source
-    $MAKE_PROG prefix=$stagedir INSTALL_DATA="/usr/local/bin/install -c -m644" install
+    generic_install DESTDIR
 }
 
 reg pack
 pack()
 {
-    generic_pack shortroot
+    generic_pack
 }
 
 reg distclean
@@ -67,42 +61,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
