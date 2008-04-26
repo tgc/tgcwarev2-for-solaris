94116d10bfa265c5b3699ad2ae6d84d1944fd3c1
Tom G. Christensen <tgc@jupiterrise.com>
2008-04-26 12:06:40 +0000

    GNU gzip 1.3.12

---
+++ gzip/build.sh
@@ -0,0 +1,68 @@
+#!/bin/env bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gzip
+version=1.3.12
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+
+reg prep
+prep()
+{
+    generic_prep
+    # Try ksh before bash when looking for POSIX compliant shell
+    setdir source
+    ${__gsed} -i 's/bash ksh/ksh bash/' configure
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS TODO NEWS README README-alpha ChangeLog
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
