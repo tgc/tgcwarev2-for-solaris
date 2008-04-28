6c59f4733d27cf2ec27afa130b458bada3b8fc56
Tom G. Christensen <tgc@jupiterrise.com>
2008-04-28 07:46:32 +0000

    gmp 4.2.2

---
+++ gmp/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gmp
+version=4.2.2
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+[ "$_os" = "sunos56" -a "$arch" = "sparc" ] && triplet="sparc-sun-solaris2.6"
+configure_args="--host=$triplet --build=$triplet $configure_args"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS COPYING COPYING.LIB NEWS README
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
