03663147771fdfb89184d7d25f6abbf187f56b99
Tom G. Christensen <tgc@jupiterrise.com>
2008-05-12 11:20:42 +0000

    git 1.5.5.1 with the basics. No http suport and no gui stuff.

--- git/build.sh
+++ git/build.sh
@@ -9,52 +9,74 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=git
-version=1.5.3.4
-pkgver=3
+version=1.5.5.1
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 source[1]=$topdir-manpages-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=git-1.5.3.4-symlinks.patch
+patch[1]=git-1.5.5.1-socklen_t.patch
+patch[2]=git-1.5.5.1-sunos56.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export topinstalldir=/usr/sbware
-export prefix=$topinstalldir
+
+# Helper to tweak makefile defines
+set_define()
+{
+	${__sed} -e "/Define $1/ s/.*/$1=$2/" Makefile > Makefile.1
+	mv Makefile.1 Makefile
+}
+
+export ICONVDIR=$prefix
+export PERL_PATH=$prefix/bin/perl
+export SHELL_PATH=$prefix/bin/bash
+no_configure=1
+make_check_target="test"
+__configure="make"
+configure_args=""
 
 reg prep
 prep()
 {
     generic_prep
     setdir source
-    perl -i -pe 's;ginstall;/usr/local/bin/install;g' Makefile
-    perl -i -pe 's;gtar;/usr/local/bin/tar;g' Makefile
-    sed -e '/Define NO_CURL/ s/.*/NO_CURL=Yes/' Makefile > Makefile.1
+    perl -i -pe "s;ginstall;${__install};g" Makefile
+    perl -i -pe "s;gtar;${__tar};g" Makefile
+    for def in NO_CURL NO_EXPAT NO_TCLTK; do
+	set_define $def YesPlease
+    done
+
+    # Set default buildflags
+    sed -e "/^LDFLAGS/ s;.*;LDFLAGS = -L$prefix/lib -R$prefix/lib;" Makefile > Makefile.1
     mv Makefile.1 Makefile
-    sed -e '/PERL_PATH =/ s;.*;PERL_PATH=/opt/csw/bin/perl;' Makefile > Makefile.1
-    mv Makefile.1 Makefile
-    sed -e '/Define NO_EXPAT/ s/.*/NO_EXPAT=Yes/' Makefile > Makefile.1
-    mv Makefile.1 Makefile
-#    sed -e '/Define NO_PERL_MAKEMAKER/ s/.*/NO_PERL_MAKEMAKER=Yes/' Makefile.1 > Makefile
-#    mv Makefile.1 Makefile
-    sed -e '/Define NO_TCLTK/ s/.*/NO_TCLTK=Yes/' Makefile > Makefile.1
-    mv Makefile.1 Makefile
-    sed -e '/^LDFLAGS/ s;.*;LDFLAGS = -L/usr/sbware/lib -R/usr/sbware/lib -L/usr/local/lib -R/usr/local/lib;' Makefile > Makefile.1
+    sed -e "/^CFLAGS/ s;\(.*\);\1 -I$prefix/include;" Makefile > Makefile.1
     mv Makefile.1 Makefile
     sed -e "/^prefix =/ s;.*;prefix = $prefix;" Makefile > Makefile.1
     mv Makefile.1 Makefile
+
 #    sed -e '/Define V=/ s/.*/V=1/' Makefile > Makefile.1
 #    mv Makefile.1 Makefile
+##    sed -e '/Define NO_PERL_MAKEMAKER/ s/.*/NO_PERL_MAKEMAKER=Yes/' Makefile.1 > Makefile
+##    mv Makefile.1 Makefile
 }
 
 reg build
 build()
 {
-    setdir source
-    $MAKE_PROG
+    generic_build
+#    setdir source
+#    ${__make}
 #    cd perl
 #    perl Makefile.PL PREFIX="${stagedir}${prefix}" INSTALLDIRS="vendor"
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg install
@@ -63,13 +85,13 @@ install()
     generic_install DESTDIR
     mkdir -p ${stagedir}${prefix}/${_mandir}
     setdir ${stagedir}${prefix}/${_mandir}
-    $TAR -xvjf ${srcfiles}/${source[1]}
+    ${__tar} -xvjf ${srcfiles}/${source[1]}
     # Hopeless, absolutely hopeless :(
-    setdir ${stagedir}${prefix}/${_libdir}/perl5/site_perl
-    mkdir -p ${stagedir}/opt/csw/lib/perl/site_perl
-    mv * ${stagedir}/opt/csw/lib/perl/site_perl
-    setdir ${stagedir}${prefix}/${_libdir}
-    rm -rf perl*
+    #setdir ${stagedir}${prefix}/${_libdir}/perl5/site_perl
+    #mkdir -p ${stagedir}/opt/csw/lib/perl/site_perl
+    #mv * ${stagedir}/opt/csw/lib/perl/site_perl
+    #setdir ${stagedir}${prefix}/${_libdir}
+    #rm -rf perl*
 }
 
 reg pack
