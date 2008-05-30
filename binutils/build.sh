18d0aebb7a7a9e9498fd4be241834bfea41fc9f1
Tom G. Christensen <tgc@jupiterrise.com>
2008-05-30 11:57:06 +0000

    Update to 2.18

--- binutils/build.sh
+++ binutils/build.sh
@@ -3,12 +3,13 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=binutils
-version=2.14
+version=2.18
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -17,18 +18,10 @@ source[0]=$topdir-$version.tar.bz2
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="Binutils - Collection of binary utilities"
-pkgcat="application"
-pkgvendor="http://www.gnu.org"
-pkgdesc="Includes tools capable of linking and managing archives"
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+configure_args="$configure_args --disable-nls --program-prefix=g"
 
 reg prep
 prep()
@@ -39,20 +32,26 @@ prep()
 reg build
 build()
 {
-    setdir stage
-    ./configure --prefix=$prefix --disable --enable-shared
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg install
 install()
 {
     generic_install DESTDIR
-    # Remove files that are overwritten by compiler packages
-    rm -f $stagedir$prefix/info/dir
-    rm -f $stagedir$prefix/info/standards.info
-    rm -f $stagedir$prefix/bin/c++filt
-    rm -f $stagedir$prefix/man/man1/c++filt.1
-    rm -f $stagedir$prefix/lib/libiberty*
+    ${__rm} -f ${stagedir}${prefix}/${_mandir}/man1/{dlltool,nlmconv,windres,windmc}*
+    # Rename gc++filt
+    ${__mv} ${stagedir}${prefix}/${_bindir}/gc++filt ${stagedir}${prefix}/${_bindir}/c++filt
+
+    doc COPYING*
+    # This copy is very old. Autoconf has a much newer copy.
+    ${__rm} -f ${stagedir}${prefix}/${_infodir}/standards.*
 }
 
 reg pack
@@ -70,42 +69,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
