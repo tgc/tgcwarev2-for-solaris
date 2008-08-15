8ebc79f63e2d1e7cf9dd89e884da89fca3275500
Tom G. Christensen <tgc@jupiterrise.com>
2008-08-15 20:26:03 +0000

    libidn 1.9 for Solaris 2.6/sparc

---
+++ libidn/build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2008 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libidn
+version=1.9
+pkgver=1
+source[0]=ftp://ftp.sunet.se/pub/gnu/libidn/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+configure_args="$configure_args --disable-static --with-libiconv-prefix=$prefix --with-libintl-prefix=$prefix"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS COPYING COPYING.LIB NEWS THANKS TODO FAQ README
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
