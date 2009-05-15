16c31f4b40f62002979e73898c937b7b3a922b80
Tom G. Christensen <tgc@jupiterrise.com>
2009-05-15 18:20:11 +0000

    Add url.

--- curl/build.sh
+++ curl/build.sh
@@ -11,7 +11,7 @@
 topdir=curl
 version=7.19.4
 pkgver=1
-source[0]=$topdir-$version.tar.bz2
+source[0]=http://curl.haxx.se/download/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
