161b69d5746bebc379494569b229b702aca094fe
Tom G. Christensen <tgc@jupiterrise.com>
2010-04-25 11:23:48 +0000

    Update to 5.0.1

--- gmp/build.sh
+++ gmp/build.sh
@@ -4,25 +4,28 @@
 # It can be used nearly unmodified with many packages
 # 
 # build.sh helper functions
-. ${BUILDPKG_BASE}/scripts/build.sh.functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=gmp
-version=4.2.4
+version=5.0.1
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/gmp/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
 # Source function library
-. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
 # Global settings
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 [ "$_os" = "sunos56" ] && triplet="${arch}-sun-solaris2.6"
 [ "$_os" = "sunos57" ] && triplet="${arch}-sun-solaris2.7"
-configure_args="--host=$triplet --build=$triplet $configure_args"
+configure_args="--host=$triplet --build=$triplet $configure_args --enable-cxx"
+# otherwise configure tests will fail since they don't respect LDFLAGS :(
+export LD_LIBRARY_PATH="$prefix/lib"
+
 
 reg prep
 prep()
@@ -47,6 +50,11 @@ install()
 {
     generic_install DESTDIR
     doc AUTHORS COPYING COPYING.LIB NEWS README
+
+    # Compat libraries
+    setdir ${prefix}/${_libdir}
+    ${__tar} -cf - libgmp.so.3* | (cd ${stagedir}${prefix}/${_libdir}; ${__tar} -xf -)
+    compat gmp 4.2.4 1 5
 }
 
 reg pack
