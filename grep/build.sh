921b3e81456ac45c1e8164f5b71012c243866417
Tom G. Christensen <tgc@jupiterrise.com>
2010-05-03 16:03:08 +0000

    Update to 2.6.3

--- grep/build.sh
+++ grep/build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
 # build.sh helper functions
-. ${BUILDPKG_BASE}/scripts/build.sh.functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=grep
-version=2.5.3
+version=2.6.3
 pkgver=1
-source[0]=$topdir-$version.tar.bz2
+source[0]=ftp://ftp.sunet.se/pub/gnu/grep/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
 
 # Source function library
-. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
 # Global settings
 export CPPFLAGS="-I$prefix/include"
@@ -44,6 +41,7 @@ reg install
 install()
 {
     generic_install DESTDIR
+    rmdir ${stagedir}${prefix}/${_libdir}
     doc NEWS COPYING AUTHORS THANKS
 }
 
