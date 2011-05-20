ea78f651baabe0931b0262be962c579e2c55dd00
Tom G. Christensen <tgc@jupiterrise.com>
2011-05-20 07:56:25 +0000

    pcre 7.6

--- pcre/build.sh
+++ pcre/build.sh
@@ -3,12 +3,13 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=pcre
-version=4.5
+version=7.6
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -17,11 +18,10 @@ source[0]=$topdir-$version.tar.bz2
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+configure_args="$configure_args --enable-utf8"
 
 reg prep
 prep()
@@ -32,22 +32,20 @@ prep()
 reg build
 build()
 {
-    configure_args='--prefix=$prefix --enable-utf8'
     generic_build
-    setdir source
-    $MAKE_PROG runtest
-    # Workaround libtool problems...
-    setdir source
-    ${SED} -e "s# libpcre.la# -L$srcdir/$topsrcdir/.libs libpcre.la#g" libpcreposix.la > libpcreposix.la.new
-    ${MV} libpcreposix.la.new libpcreposix.la
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg install
 install()
 {
     generic_install DESTDIR
-    doc NEWS AUTHORS ChangeLog LICENCE
-    ${RM} -f ${stagedir}${prefix}/${_libdir}/*.la
+    setdir ${stagedir}${prefix}/${_docdir}/pcre
 }
 
 reg pack
@@ -65,42 +63,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
