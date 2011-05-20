8f97a7c67d55fda4688cb0a07b2b27fda6f5ef9d
Tom G. Christensen <tgc@jupiterrise.com>
2011-05-20 12:05:49 +0000

    Update to curl 7.21.6

--- curl/build.sh
+++ curl/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=7.21.1
+version=7.21.6
 pkgver=1
 source[0]=http://curl.haxx.se/download/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
