d1e8ba3efc3f301cf38f608f0547e45d6057697b
Tom G. Christensen <tgc@jupiterrise.com>
2011-05-21 14:14:34 +0000

    Update to mpc 0.9

--- mpc/build.sh
+++ mpc/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=mpc
-version=0.8.1
+version=0.9
 pkgver=1
 source[0]=http://multiprecision.org/mpc/download/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
