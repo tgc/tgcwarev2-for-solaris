523fdb9242f7a33232173dba3184b566df1f05e5
Tom G. Christensen <tgc@jupiterrise.com>
2011-06-03 18:50:35 +0000

    Update to autoconf 2.68

--- autoconf/build.sh
+++ autoconf/build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
 # build.sh helper functions
-. ${BUILDPKG_BASE}/scripts/build.sh.functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=autoconf
-version=2.62
+version=2.68
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
 # Source function library
-. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
 reg prep
 prep()
