043e914a11b15eefbba09d0286b51e671c023feb
Tom G. Christensen <tgc@jupiterrise.com>
2011-06-03 14:55:44 +0000

    xzutils 5.0.3

---
+++ xzutils/build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=xz
+version=5.0.3
+pkgver=1
+source[0]=http://tukaani.org/xz/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/lib"
+export LDFLAGS="-L/usr/tgcware/lib -R/usr/tgcware/lib"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    ${__mv} ${stagedir}${prefix}/${_docdir}/xz ${stagedir}${prefix}/${_vdocdir}
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
