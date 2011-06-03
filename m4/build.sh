a35d27347179ce29f49c1476f5b307b74585084f
Tom G. Christensen <tgc@jupiterrise.com>
2011-06-03 18:50:05 +0000

    Update to m4 1.4.16

--- m4/build.sh
+++ m4/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=m4
-version=1.4.15
+version=1.4.16
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/m4/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
