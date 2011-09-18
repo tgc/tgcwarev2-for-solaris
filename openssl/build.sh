6c8924cf2a622f161c036760ad9625cb1395e860
Tom G. Christensen <tgc@jupiterrise.com>
2011-09-18 08:46:53 +0000

    Update to openssl 1.0.0e

--- openssl/build.sh
+++ openssl/build.sh
@@ -6,8 +6,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssl
-version=1.0.0d
-pkgver=3
+version=1.0.0e
+pkgver=4
 source[0]=http://openssl.org/source/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
