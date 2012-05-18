3d0023ed4e48be167fd012edb4ce2cfc0c2a9f49
Tom G. Christensen <tgc@jupiterrise.com>
2012-05-18 16:35:26 +0200

    dejagnu: update to 1.5

--- dejagnu/build.sh
+++ dejagnu/build.sh
@@ -6,9 +6,9 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=dejagnu
-version=1.4.4
+version=1.5
 pkgver=1
-source[0]=$topdir-$version.tar.gz
+source[0]=ftp://ftp.sunet.se/pub/gnu/dejagnu/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
 
