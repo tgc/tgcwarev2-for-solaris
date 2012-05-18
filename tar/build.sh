7f29d7af79d8dd3d5a4a8df1742c471c560a0ff2
Tom G. Christensen <tgc@jupiterrise.com>
2012-05-18 15:04:28 +0200

    tar: update to 1.26

--- tar/build.sh
+++ tar/build.sh
@@ -6,9 +6,9 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=tar
-version=1.20
+version=1.26
 pkgver=1
-source[0]=$topdir-$version.tar.bz2
+source[0]=ftp://ftp.sunet.se/pub/gnu/tar/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
