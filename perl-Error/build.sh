9606c2e746756802e448a6e182bb0c7f57c1c734
Tom G. Christensen <tgc@jupiterrise.com>
2012-05-18 15:06:59 +0200

    perl-Error: update to 0.1718

--- perl-Error/build.sh
+++ perl-Error/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=Error
-version=0.17015
+version=0.17018
 pkgver=1
 source[0]=http://search.cpan.org/CPAN/authors/id/S/SH/SHLOMIF/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
