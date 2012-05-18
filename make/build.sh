e533d2aac8dd59a542a7ce1602a4bc9a55a57a23
Tom G. Christensen <tgc@jupiterrise.com>
2012-05-18 15:08:24 +0200

    make: update to 3.82

--- make/build.sh
+++ make/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=make
-version=3.81
+version=3.82
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
