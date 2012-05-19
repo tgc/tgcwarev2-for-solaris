1c1aba1099fb3bdeef748e8aa69ca955cc144e37
Tom G. Christensen <tgc@jupiterrise.com>
2012-05-19 12:35:34 +0200

    binutils: update to 2.22

--- binutils/build.sh
+++ binutils/build.sh
@@ -6,8 +6,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=binutils
-version=2.18
-pkgver=1
+version=2.22
+pkgver=2
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -18,7 +18,7 @@ source[0]=$topdir-$version.tar.bz2
 # Global settings
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
-configure_args="$configure_args --disable-nls --program-prefix=g"
+configure_args="$configure_args --disable-nls --disable-werror --program-prefix=g"
 
 reg prep
 prep()
@@ -42,13 +42,15 @@ reg install
 install()
 {
     generic_install DESTDIR
-    ${__rm} -f ${stagedir}${prefix}/${_mandir}/man1/{dlltool,nlmconv,windres,windmc}*
-    # Rename gc++filt
-    ${__mv} ${stagedir}${prefix}/${_bindir}/gc++filt ${stagedir}${prefix}/${_bindir}/c++filt
+    {__rm} -f ${stagedir}${prefix}/${_mandir}/man1/{dlltool,nlmconv,windres,windmc}*
 
+    ${__mkdir} -p ${stagedir}${prefix}/../bin
+    setdir ${stagedir}${prefix}/../bin
+    for cmd in c++filt objdump objcopy
+    do
+      ${__ln_s} ../binutils/bin/g$cmd $cmd
+    done
     doc COPYING*
-    # This copy is very old. Autoconf has a much newer copy.
-    ${__rm} -f ${stagedir}${prefix}/${_infodir}/standards.*
 }
 
 reg pack
