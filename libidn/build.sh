db0bee598dfe27410c4353abe4d3a5651dcfa5a7
Tom G. Christensen <tgc@jupiterrise.com>
2012-06-05 13:59:33 +0200

    libidn: update to 1.25

--- libidn/build.sh
+++ libidn/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=libidn
-version=1.24
+version=1.25
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/libidn/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -44,8 +44,9 @@ install()
     generic_install DESTDIR
     doc AUTHORS COPYING* NEWS THANKS TODO FAQ README
     # It's compatible with previous build
-    compat lidn 1.10 1 1
-    compat lidn 1.18 1 1
+    compat libidn 1.10 1 1
+    compat libidn 1.18 1 1
+    compat libidn 1.24 1 1
 }
 
 reg pack
