5f38ae3e90d0723b0650662e8bd2aae5ddf84930
Tom G. Christensen <tgc@jupiterrise.com>
2012-06-05 16:21:26 +0200

    git: update to 1.7.10.4

--- git/build.sh
+++ git/build.sh
@@ -6,10 +6,10 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=git
-version=1.7.2.3
+version=1.7.10.4
 pkgver=1
-source[0]=$topdir-$version.tar.bz2
-source[1]=$topdir-manpages-$version.tar.bz2
+source[0]=http://git-core.googlecode.com/files/$topdir-$version.tar.gz
+source[1]=http://git-core.googlecode.com/files/$topdir-manpages-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=git-1.7.2.3-symlinks.patch
 
@@ -20,7 +20,7 @@ patch[0]=git-1.7.2.3-symlinks.patch
 
 export ICONVDIR=$prefix
 export PERL_PATH=$prefix/bin/perl
-export SHELL_PATH=$prefix/bin/bash
+export CC=gcc
 no_configure=1
 __configure="make"
 configure_args=""
@@ -34,19 +34,14 @@ prep()
 {
     generic_prep
     setdir source
-    # Solaris 2.6
+    # Solaris 2.8
     cat <<EOF> config.mak
 NO_EXPAT=YesPlease
-NO_TCLTK=YesPlease
 BASIC_CFLAGS += -I/usr/tgcware/include
 BASIC_LDFLAGS += -L/usr/tgcware/lib -Wl,-R,/usr/tgcware/lib
-EXTLIBS += -lresolv
 NEEDS_LIBICONV = YesPlease
 NO_PYTHON = YesPlease
-# missing PTHREAD_MUTEX_RECURSIVE with posix95 pthreads
-NO_PTHREADS = YesPlease
 TAR = /usr/tgcware/bin/tar
-INSTALL = /usr/tgcware/bin/install
 prefix=$prefix
 EOF
 
@@ -68,9 +63,10 @@ reg install
 install()
 {
     generic_install DESTDIR
+    mkdir -p ${stagedir}${prefix}/${_mandir}
     setdir ${stagedir}${prefix}/${_mandir}
-    ${__tar} -xjf $(get_source_absfilename "${source[1]}")
-    doc COPYING Documentation/RelNotes-${version}.txt README
+    ${__tar} -xf $(get_source_absfilename "${source[1]}")
+    doc COPYING Documentation/RelNotes/${version}.txt README
 
     # fix git symlink
     ${__rm} -f ${stagedir}${prefix}/libexec/git-core/git
@@ -80,16 +76,6 @@ install()
     ${__rm} -rf ${stagedir}${prefix}/${_libdir}/perl5/5.*
     ${__rm} -rf ${stagedir}${prefix}/${_libdir}/perl5/site_perl/*/*solaris
     ${__rm} -f ${stagedir}${prefix}/${_libdir}/perl5/site_perl/*/Error.pm
-
-    mkdir -p ${stagedir}${prefix}/${_mandir}
-    setdir ${stagedir}${prefix}/${_mandir}
-    ${__tar} -xvjf ${srcfiles}/${source[1]}
-    # Hopeless, absolutely hopeless :(
-    #setdir ${stagedir}${prefix}/${_libdir}/perl5/site_perl
-    #mkdir -p ${stagedir}/opt/csw/lib/perl/site_perl
-    #mv * ${stagedir}/opt/csw/lib/perl/site_perl
-    #setdir ${stagedir}${prefix}/${_libdir}
-    #rm -rf perl*
 }
 
 reg pack
