db7abb289499c1250d4a50d930783b72bbf4e42c
Tom G. Christensen <tgc@jupiterrise.com>
2012-12-01 11:28:17 +0100

    gcc31: set bugurl and vendor in prep stage

--- gcc31/build.sh
+++ gcc31/build.sh
@@ -31,11 +31,6 @@ reg prep
 prep()
 {
     generic_prep
-}
-
-reg build
-build()
-{
     setdir source
     # Set bugurl and vendor version
     ${__gsed} -i "/GCCBUGURL/s|URL:[^>]*|URL:$gccbugurl|" gcc/system.h
@@ -43,8 +38,12 @@ build()
     ${__gsed} -i "s/(release)/($gccpkgversion)/" gcc/version.c gcc/f/version.c
     # not gccpkgversion, because the version string will exceed max length
     ${__gsed} -i "s/(release)/(${version}-${pkgver})/" gcc/ada/gnatvsn.ads
-    #
-    ${__mkdir} -p ../$objdir
+}
+
+reg build
+build()
+{
+    ${__mkdir} -p ${srcdir}/$objdir
     echo "$__configure $configure_args"
     generic_build ../$objdir
     # Build gnat
