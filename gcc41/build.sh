889e22d7bd69988071d9ce62c8c5f9092475e732
Tom G. Christensen <tgc@jupiterrise.com>
2012-12-08 15:58:13 +0100

    gcc41: fix depends

--- gcc41/build.sh
+++ gcc41/build.sh
@@ -5,12 +5,10 @@
 #
 ###########################################################
 # Check the following 4 variables before running the script
-snapshot=
 topdir=gcc
 version=4.1.2
 pkgver=2
 source[0]=ftp://ftp.sunet.se/pub/gnu/gcc/releases/$topdir-$version/$topdir-$version.tar.bz2
-[ -n "$snapshot" ] && source[0]=$topdir-$version-$snapshot.tar.bz2
 ## If there are no patches, simply comment this
 patch[0]=gcc-4.1.2-new-makeinfo.patch
 patch[1]=gcc-4.1.2-new-gas.patch
