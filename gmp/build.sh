6f5156bb0e9a3157db678736de0f7e4bddb72feb
Tom G. Christensen <tgc@jupiterrise.com>
2012-12-14 17:34:38 +0100

    gmp: rebuild

--- gmp/build.sh
+++ gmp/build.sh
@@ -7,7 +7,7 @@
 # Check the following 4 variables before running the script
 topdir=gmp
 version=5.0.5
-pkgver=3
+pkgver=4
 source[0]=ftp://ftp.sunet.se/pub/gnu/gmp/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -15,14 +15,13 @@ source[0]=ftp://ftp.sunet.se/pub/gnu/gmp/$topdir-$version.tar.bz2
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
+# Get host triplet
+. ${BUILDPKG_BASE}/gcc/build.sh.gcc.cpu
+
 # Global settings
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
-[ "$arch" = "sparc" ] &&  vendor="sun" || vendor="pc"
-[ "$_os" = "sunos58" -a "$arch" = "sparc" ] && triplet="${arch}-${vendor}-solaris2.8"
-# Use i686 instructions
-[ "$_os" = "sunos58" -a "$arch" = "i386" ] && triplet="pentiumpro-${vendor}-solaris2.8"
-configure_args="--host=$triplet --build=$triplet $configure_args" # --enable-cxx
+configure_args="--host=$gmp_host --build=$gmp_host $configure_args"
 
 reg prep
 prep()
@@ -48,8 +47,7 @@ install()
     generic_install DESTDIR
     doc AUTHORS COPYING COPYING.LIB NEWS README
 
-    compat gmp 5.0.4 1 1
-    compat gmp 5.0.5 1 2
+    compat gmp 5.0.5 1 3
 }
 
 reg pack
