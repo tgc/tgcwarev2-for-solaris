0a4799e3c38aab8c3f701d8fcd9c9dbb8a93a1fd
Tom G. Christensen <tgc@jupiterrise.com>
2013-01-27 10:44:26 +0100

    patch: update to 2.7.1

--- patch/build.sh
+++ patch/build.sh
@@ -6,11 +6,11 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=patch
-version=2.6.1
+version=2.7.1
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/patch/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-patch[0]=patch-2.6.1-missing-strnlen_c.patch
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -19,18 +19,12 @@ patch[0]=patch-2.6.1-missing-strnlen_c.patch
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 ac_overrides="ac_cv_path_ed_PROGRAM=/usr/bin/ed"
+gnu_link_progs="patch"
 
 reg prep
 prep()
 {
     generic_prep
-    # testsuite expects gnutools
-    setdir source
-    ${__gsed} -i 's/cat/gcat/g' tests/*
-    ${__gsed} -i 's/touch/gtouch/g' tests/*
-    ${__gsed} -i 's/seq/gseq/g' tests/*
-    ${__gsed} -i 's/mktemp/gmktemp/g' tests/*
-    ${__gsed} -i 's/date/gdate/g' tests/*
 }
 
 reg build
