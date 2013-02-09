f5cce93a1f8263800836bdffe9d716a106acb5a5
Tom G. Christensen <tgc@jupiterrise.com>
2013-02-09 14:24:45 +0100

    gawk: new package, gawk 4.0.2

---
+++ gawk/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gawk
+version=4.0.2
+pkgver=1
+source[0]=ftp://ftp.sunet.se/pub/gnu/${topdir}/${topdir}-${version}.tar.xz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS COPYING FUTURES LIMITATIONS NEWS POSIX.STD PROBLEMS README
+
+   # We don't want versioned hardlinks
+   ${__rm} -f ${stagedir}${prefix}/${_bindir}/*-${version}
+   # No awk link either (will be in $_gnudir instead)
+   ${__rm} -f ${stagedir}${prefix}/${_bindir}/awk
+
+   # Setup gnu link
+   gnu_link gawk
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
