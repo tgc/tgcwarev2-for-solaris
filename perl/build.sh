9a16f151696392b34c1c3c9e24f36443f544b3de
Tom G. Christensen <tgc@jupiterrise.com>
2013-02-14 17:17:01 +0100

    perl: update to 5.16.2

--- perl/build.sh
+++ perl/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=perl
-version=5.14.2
+version=5.16.2
 pkgver=1
 source[0]=http://www.cpan.org/src/5.0/perl-${version}.tar.gz
 # If there are no patches, simply comment this
@@ -14,13 +14,13 @@ source[0]=http://www.cpan.org/src/5.0/perl-${version}.tar.gz
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
- 
+
 # Global settings
 make_check_target="test"
 __configure="sh Configure"
 [ "$arch" = "sparc" ] && arch_name="sun4-solaris"
 [ "$arch" = "i386" ] && arch_name="i86pc-solaris"
-configure_args="-Dcc='gcc' -Darchname=${arch_name} -Dprefix=$prefix -Dmyhostname=localhost -Dcf_by='Tom G. Christensen' -Dcf_email='swpkg@jupiterrise.com' -Dperladmin=root@localhost -Dinstallprefix=${stagedir}${prefix} -Dman3ext=3pm -Uinstallusrbinperl -Dpager='/usr/bin/more' -Dlocincpth='/usr/tgcware/include' -Dloclibpth='/usr/tgcware/lib' -des"
+configure_args="-Dcc='gcc' -Accflags=-fno-stack-protector -Darchname=${arch_name} -Dprefix=$prefix -Dmyhostname=localhost -Dcf_by='Tom G. Christensen' -Dcf_email='swpkg@jupiterrise.com' -Dperladmin=root@localhost -Dinstallprefix=${stagedir}${prefix} -Dman3ext=3pm -Uinstallusrbinperl -Dpager='/usr/bin/more' -Dlocincpth='/usr/tgcware/include' -Dloclibpth='/usr/tgcware/lib' -des"
 
 reg prep
 prep()
@@ -32,7 +32,7 @@ reg build
 build()
 {
     setdir source
-    $__configure -Dcc='gcc' -Darchname=${arch_name} -Dprefix=$prefix -Dmyhostname=localhost -Dcf_by='Tom G. Christensen' -Dcf_email='swpkg@jupiterrise.com' -Dperladmin=root@localhost -Dinstallprefix=${stagedir}${prefix} -Dman3ext=3pm -Uinstallusrbinperl -Dpager='/usr/bin/more' -Dlocincpth='/usr/tgcware/include' -Dloclibpth='/usr/tgcware/lib' -des
+    $__configure -Dcc='gcc' -Accflags=-fno-stack-protector -Darchname=${arch_name} -Dprefix=$prefix -Dmyhostname=localhost -Dcf_by='Tom G. Christensen' -Dcf_email='swpkg@jupiterrise.com' -Dperladmin=root@localhost -Dinstallprefix=${stagedir}${prefix} -Dman3ext=3pm -Uinstallusrbinperl -Dpager='/usr/bin/more' -Dlocincpth='/usr/tgcware/include' -Dloclibpth='/usr/tgcware/lib' -des
     ${__make} LDDLFLAGS="-shared -L$prefix/lib -R$prefix/lib" CLDFLAGS="-L$prefix/lib -R$prefix/lib"
 }
 
@@ -62,7 +62,6 @@ install()
         fi
     done
     ${__rm} -f ${stagedir}${prefix}/${_bindir}/perl$version
-    compat perl 5.8.8 1 1
 }
 
 reg pack
