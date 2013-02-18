ab6ac1b903519368f3b61f4eb4fb62193eeac49c
Tom G. Christensen <tgc@jupiterrise.com>
2013-02-18 18:25:01 +0100

    expat: new package, expat 2.1.0

---
+++ expat/build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=expat
+version=2.1.0
+pkgver=1
+source[0]=http://downloads.sourceforge.net/project/expat/expat/${version}/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+export LDFLAGS="-L/usr/tgcware/lib -R/usr/tgcware/lib"
+export CPPFLAGS="-I/usr/tgcware/include"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING Changes README
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
