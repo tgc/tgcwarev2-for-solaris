11d245aa067b9346e544d72aa8a4e011a31f8fa7
Tom G. Christensen <tgc@jupiterrise.com>
2013-05-04 13:33:10 +0200

    wget: fix build on Solaris 7

--- wget/build.sh
+++ wget/build.sh
@@ -7,10 +7,11 @@
 # Check the following 4 variables before running the script
 topdir=wget
 version=1.14
-pkgver=1
+pkgver=2
 source[0]=ftp://ftp.sunet.se/pub/gnu/wget/$topdir-$version.tar.xz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=wget-1.14-fix-gnulib-locale_h.patch
+patch[1]=wget-1.14-no-ipv6.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
