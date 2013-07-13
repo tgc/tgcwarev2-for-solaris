7f56502143b5347def43d39c7c68e1934131ea7c
Tom G. Christensen <tgc@jupiterrise.com>
2013-07-13 14:51:42 +0200

    cloog015: new package, CLooG 0.15.11

---
+++ cloog015/build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=cloog-ppl
+version=0.15.11
+pkgver=1
+source[0]=ftp://gcc.gnu.org/pub/gcc/infrastructure/${topdir}-${version}.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Get gcc arch setting
+. ${BUILDPKG_BASE}/gcc/build.sh.gcc.cpu
+
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+configure_args="$configure_args --disable-static --with-ppl"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    docs_for libcloog0 LICENSE README
+    ${__mv} ${stagedir}${prefix}/${_bindir}/cloog ${stagedir}${prefix}/${_bindir}/cloog-ppl
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
