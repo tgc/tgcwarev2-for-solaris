61bd7115696779854af6fc1a0b3fc0b06c8dd137
Tom G. Christensen <tgc@jupiterrise.com>
2013-10-12 15:15:54 +0200

    vim: update to 7.4.52

--- vim/build.sh
+++ vim/build.sh
@@ -6,58 +6,97 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=vim
-version=7.3
+shortver=7.4
+version=${shortver}.52
 pkgver=1
-source[0]=ftp://ftp.vim.org/pub/vim/unix/$topdir-${version}.tar.bz2
+source[0]=ftp://ftp.vim.org/pub/vim/unix/$topdir-$shortver.tar.bz2
 # If there are no patches, simply comment this
-# Generate rough patchlist like this:
-# grep -v Win32 README | grep -v VMS | grep -v Mac | grep -v \.gz |grep -v \(extra|awk '{ print $2 }'|grep 7.1
-# Unfortunately the markers are misleading and several Mac, Win32 and extra patches need also to be applied
-# num=0; for i in `grep -v Win32 README | grep -v VMS | grep -v Mac | grep -v \.gz |grep -v \(extra|awk '{ print $2 }'|grep 7.1`; do echo "patch[$num]=$i"; let num=num+1; done
+patch[0]=   # buildpkg does not handle sparse patch array
+patch[1]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.001
+patch[2]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.002
+patch[3]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.003
+patch[4]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.004
+patch[5]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.005
+patch[6]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.006
+patch[7]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.007
+patch[8]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.008
+patch[9]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.009
+patch[10]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.010
+patch[11]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.011
+patch[12]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.012
+patch[13]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.013
+patch[14]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.014
+patch[15]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.015
+patch[16]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.016
+patch[17]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.017
+patch[18]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.018
+patch[19]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.019
+patch[20]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.020
+patch[21]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.021
+patch[22]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.022
+patch[23]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.023
+patch[24]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.024
+patch[25]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.025
+patch[26]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.026
+patch[27]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.027
+patch[28]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.028
+patch[29]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.029
+patch[30]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.030
+patch[31]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.031
+patch[32]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.032
+patch[33]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.033
+patch[34]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.034
+patch[35]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.035
+patch[36]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.036
+patch[37]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.037
+patch[38]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.038
+patch[39]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.039
+patch[40]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.040
+patch[41]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.041
+patch[42]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.042
+patch[43]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.043
+patch[44]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.044
+patch[45]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.045
+patch[46]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.046
+patch[47]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.047
+patch[48]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.048
+patch[49]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.049
+patch[50]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.050
+patch[51]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.051
+patch[52]=ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.052
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
 # Global settings
-# We need to override this
-topsrcdir=vim73
-patchdir=$srcfiles/vim-${version}-patches
-patch_prefix="-p0"
-# Augment version based on patchlevel
-pver=$(cd $patchdir && ls ${version}*|sort -n|tail -1)
-real_version=$version
-version=$pver
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
-# What gui should we build?
-gui=motif
-configure_args='--prefix=$prefix --without-local-dir --enable-gui=$gui --with-features=huge --enable-multibyte --disable-perlinterp --disable-pythoninterp --disable-tclinterp --with-compiledby="<swpkg@jupiterrise.com>" --disable-netbeans'
+basic_args=(--prefix=$prefix --without-local-dir --with-features=huge --enable-multibyte --disable-perlinterp --disable-pythoninterp --disable-tclinterp --with-compiledby="<swpkg@jupiterrise.com>" --disable-netbeans)
+# Do not let scripts add a dependency on perl
+ignore_deps="TGCperl"
+# We need to override these
+topsrcdir=vim74
+patch_prefix="-p0"
 
 reg prep
 prep()
 {
     generic_prep
-    # Patch
-    setdir source
-    for p in $(ls $patchdir/${real_version}*);
-    do
-      echo "Applying patch $p"
-      ${__patch} -Es $patch_prefix < $p
-    done
 }
 
 reg build
 build()
 {
     # First build a gui version
-    gui=motif
+    configure_args=("${basic_args[@]}" --enable-gui=motif)
     generic_build
     # Save the gui binary for later
     setdir source
     ${__cp} src/vim src/gvim
     setdir source
     ${__make} clean
-    gui="no --with-x=no"
+    # Build without gui
+    configure_args=("${basic_args[@]}" --enable-gui=no --with-x=no)
     generic_build
 }
 
@@ -70,6 +109,7 @@ check()
 reg install
 install()
 {
+    configure_args=("${basic_args[@]}" --enable-gui=motif)
     generic_install DESTDIR
     setdir source
     ${__cp} src/gvim ${stagedir}${prefix}/${_bindir}
@@ -86,7 +126,7 @@ install()
     #setdir ${stagedir}${prefix}/${_sharedir}/vim/vim71/lang/
     #${__mv} "menu_chinese(gb)_gb.936.vim" "menu_chinese_gb__gb.936.vim"
     #${__mv} "menu_chinese(taiwan)_taiwan.950.vim" "menu_chinese_taiwan__taiwan.950.vim"
-    ${__rm} -rf ${stagedir}${prefix}/${_mandir}/{fr,it,pl,ru}*
+    ${__rm} -rf ${stagedir}${prefix}/${_mandir}/{fr,it,pl,ru,ja}*
 }
 
 reg pack
