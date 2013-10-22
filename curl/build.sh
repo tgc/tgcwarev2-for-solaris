9975e58e369e8afde7a8bda03caf1719733cb2aa
Tom G. Christensen <tgc@jupiterrise.com>
2013-10-22 21:00:31 +0200

    curl: update to 7.33.0

--- curl/build.sh
+++ curl/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=7.32.0
+version=7.33.0
 pkgver=1
 source[0]=http://curl.haxx.se/download/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
