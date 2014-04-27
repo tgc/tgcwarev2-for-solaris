157fa1c23446bbf2ae0cc6c646e564b8c722a622
Tom G. Christensen <tgc@jupiterrise.com>
2014-04-27 15:46:19 +0200

    gcc49: new package, gcc 4.9.0

---
+++ gcc49/build.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gcc
+version=4.9.0
+pkgver=1
+source[0]=ftp://ftp.sunet.se/pub/gnu/gcc/releases/$topdir-$version/$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+topsrcdir=gcc-4.9.0
+
+# Common settings for gcc
+. ${BUILDPKG_BASE}/gcc/build.sh.gcc.common
+
+# Global settings
+java_libver=15
+
+# This compiler is bootstrapped with gcc 4.8.x
+export PATH=/usr/tgcware/gcc48/bin:$PATH
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    setup_tools
+    ${__mkdir} -p ${srcdir}/$objdir
+    generic_build ../$objdir
+}
+
+reg install
+install()
+{
+    clean stage
+    setdir ${srcdir}/${objdir}
+    ${__make} DESTDIR=$stagedir install
+    custom_install=1
+    generic_install
+    ${__find} ${stagedir} -name '*.la' -print | ${__xargs} ${__rm} -f
+
+    # Remove libffi
+    ${__find} ${stagedir} -type l -name 'libffi*' -print | ${__xargs} ${__rm} -f
+    ${__find} ${stagedir} -type f -name 'libffi*' -print | ${__xargs} ${__rm} -f
+    ${__find} ${stagedir} -type f -name 'ffi*.h' -print | ${__xargs} ${__rm} -f
+    ${__find} ${stagedir} -type f -name 'ffi*.3' -print | ${__xargs} ${__rm} -f
+    # man3 is now empty
+    ${__rmdir} ${stagedir}${prefix}/man/man3
+
+    # libquadmath is not available on sparc but documentation is installed
+    [ "$arch" = "sparc" ] && ${__rm} -f ${stagedir}${prefix}/info/libquadmath.info
+
+    # Rearrange libraries
+    redo_libs
+
+    # Turn all the hardlinks in bin into symlinks
+    redo_bin
+
+    # Place share/docs in the regular location
+    prefix=$topinstalldir
+    doc COPYING* MAINTAINERS NEWS
+
+    # Compatibility information
+    case $build_arch in
+	sparc|i386)
+	    for lib in gcc_s1 stdc++6 ssp0 gomp1 atomic1 itm1 quadmath0 gfortran3 objc4
+	    do
+		compat lib$lib 4.8.2 1 9
+		compat lib$lib 4.8.3 1 9
+	    done
+	    ;;
+    esac
+}
+
+reg check
+check()
+{
+    setdir source
+    setdir ../$objdir
+    if [ $multilib -eq 0 ]; then
+	${__make} -k check
+    else
+	${__make} -k RUNTESTFLAGS="--target_board='unix{,$multilib_testopt}'" check
+    fi
+}
+
+reg pack
+pack()
+{
+    iprefix=${topdir}${abbrev_majorminor}
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    META_CLEAN="$META_CLEAN compver.*"
+    clean distclean
+    ${__rm} -rf $srcdir/$objdir
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
