104531d579b1c1b4dad812e75a2ffd92eab8e9cb
Tom G. Christensen <tgc@jupiterrise.com>
2014-06-11 18:31:34 +0200

    openssl: update to 1.0.1h

--- openssl/build.sh
+++ openssl/build.sh
@@ -6,8 +6,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssl
-version=1.0.1g
-pkgver=3
+version=1.0.1h
+pkgver=4
 source[0]=http://openssl.org/source/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -107,6 +107,7 @@ install()
     compat openssl 1.0.1c 4 4
     compat openssl 1.0.1e 1 1
     compat openssl 1.0.1f 2 2
+    compat openssl 1.0.1g 3 3
 }
 
 reg pack
