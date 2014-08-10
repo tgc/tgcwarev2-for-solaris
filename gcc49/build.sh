77593ac7a7a6d71b54fdc68c5b6023b63c6f80c4
Tom G. Christensen <tgc@jupiterrise.com>
2014-08-10 17:18:18 +0200

    gcc49: update to 4.9.1

--- gcc49/build.sh
+++ gcc49/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=gcc
-version=4.9.0
+version=4.9.1
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/gcc/releases/$topdir-$version/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -14,8 +14,6 @@ source[0]=ftp://ftp.sunet.se/pub/gnu/gcc/releases/$topdir-$version/$topdir-$vers
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
-
-topsrcdir=gcc-4.9.0
 
 # Common settings for gcc
 . ${BUILDPKG_BASE}/gcc/build.sh.gcc.common
@@ -78,6 +76,11 @@ install()
 	    do
 		compat lib$lib 4.8.2 1 9
 		compat lib$lib 4.8.3 1 9
+		compat lib$lib 4.9.0 1 9
+	    done
+	    for lib in go5 cilkrts5
+	    do
+		compat lib$lib 4.9.0 1 9
 	    done
 	    ;;
     esac
