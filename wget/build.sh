a8708dcf0cf0f5f318a5a0fef23a16c3bc92f894
Tom G. Christensen <tgc@jupiterrise.com>
2015-03-19 21:05:30 +0100

    wget: update to 1.16.2

--- wget/build.sh
+++ wget/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=wget
-version=1.15
+version=1.16.2
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/wget/$topdir-$version.tar.xz
 # If there are no patches, simply comment this
@@ -18,7 +18,7 @@ source[0]=ftp://ftp.sunet.se/pub/gnu/wget/$topdir-$version.tar.xz
 # Global settings
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
-configure_args+=(--with-ssl=openssl)
+configure_args+=(--with-ssl=openssl --with-libssl-prefix=$prefix)
 
 reg prep
 prep()
