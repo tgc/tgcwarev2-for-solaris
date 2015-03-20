e4b21bbfb975b48fb13f56994119897b91d0c8aa
Tom G. Christensen <tgc@jupiterrise.com>
2015-03-20 09:33:47 +0100

    xzutils: update to 5.2.1

--- xzutils/build.sh
+++ xzutils/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=xz
-version=5.0.5
+version=5.2.1
 pkgver=1
 source[0]=http://tukaani.org/xz/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -45,6 +45,7 @@ install()
     generic_install DESTDIR
     ${__mv} ${stagedir}${prefix}/${_docdir}/xz ${stagedir}${prefix}/${_vdocdir}
     compat xzutils 5.0.4 1 1
+    compat xzutils 5.0.5 1 1
 }
 
 reg pack
