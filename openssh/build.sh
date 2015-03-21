3b279c5e0c70038af43fac2457de8190b2053ca3
Tom G. Christensen <tgc@jupiterrise.com>
2015-03-21 13:53:20 +0100

    openssh: update to 6.8p1

--- openssh/build.sh
+++ openssh/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssh
-version=6.7p1
+version=6.8p1
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/OpenBSD/OpenSSH/portable/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
