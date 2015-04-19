c08c362006e410abb713f9f3babdbab0182421a0
Tom G. Christensen <tgc@jupiterrise.com>
2015-04-19 17:59:15 +0200

    readline: update to 6.3p8

--- readline/build.sh
+++ readline/build.sh
@@ -6,11 +6,18 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=readline
-version=5.2
+version=6.3
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/readline/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=readline63-001
+patch[1]=readline63-002
+patch[2]=readline63-003
+patch[3]=readline63-004
+patch[4]=readline63-005
+patch[5]=readline63-006
+patch[6]=readline63-007
+patch[7]=readline63-008
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -19,6 +26,7 @@ source[0]=ftp://ftp.sunet.se/pub/gnu/readline/$topdir-$version.tar.gz
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 configure_args+=(--disable-static)
+patch_prefix=-p0
 
 reg prep
 prep()
@@ -42,6 +50,9 @@ reg install
 install()
 {
     generic_install DESTDIR
+    ${__rm} -f ${stagedir}${prefix}/${_docdir}/readline/*
+    ${__rmdir} ${stagedir}${prefix}/${_docdir}/readline
+    ${__rmdir} ${stagedir}${prefix}/${_bindir}
     doc NEWS CHANGES COPYING
 }
 
