b81c1f9bc1b38b29c3a5de411dbdb340565709cd
Tom G. Christensen <tgc@jupiterrise.com>
2015-04-25 12:17:32 +0200

    gmp: update to 6.0.0a

--- gmp/build.sh
+++ gmp/build.sh
@@ -6,9 +6,10 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=gmp
-version=5.1.3
+real_version=6.0.0
+version=${real_version}a
 pkgver=1
-source[0]=ftp://ftp.sunet.se/pub/gnu/gmp/$topdir-$version.tar.bz2
+source[0]=ftp://ftp.sunet.se/pub/gnu/gmp/$topdir-$version.tar.xz
 # If there are no patches, simply comment this
 patch[0]=gmp-5.1.2-no-c99-trunc.patch
 
@@ -23,6 +24,7 @@ export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 export LD_OPTIONS="-R$prefix/lib"
 configure_args=(--host=$gmp_host --build=$gmp_host "${configure_args[@]}" --enable-cxx)
+topsrcdir=${topdir}-${real_version}
 
 reg prep
 prep()
@@ -46,10 +48,11 @@ reg install
 install()
 {
     generic_install DESTDIR
-    doc AUTHORS COPYING COPYING.LIB NEWS README
+    doc AUTHORS COPYING COPYING.LESSERv3 NEWS README
 
     compat gmp 5.0.5 1 4
     compat gmp 5.1.2 1 1
+    compat gmp 5.1.3 1 1
 }
 
 reg pack
