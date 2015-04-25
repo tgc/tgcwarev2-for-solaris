109408c9cb97c1571d1aedd93af34de16c625e48
Tom G. Christensen <tgc@jupiterrise.com>
2015-04-25 14:35:54 +0200

    curl: update to 7.42.0

--- curl/build.sh
+++ curl/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=7.41.0
+version=7.42.0
 pkgver=1
 source[0]=http://curl.haxx.se/download/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -54,6 +54,7 @@ install()
     compat curl 7.35.0 1 1
     compat curl 7.36.0 1 1
     compat curl 7.38.0 1 1
+    compat curl 7.41.0 1 1
 }
 
 reg pack
