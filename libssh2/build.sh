d78ddfb6f5b65a0d6edb9fdf6aed6d29f6c996a3
Tom G. Christensen <tgc@jupiterrise.com>
2016-02-21 16:33:59 +0100

    libssh2: new package, libssh2 1.6.0

---
+++ libssh2/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libssh2
+version=1.6.0
+pkgver=1
+source[0]=http://www.libssh2.org/download/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+configure_args+=(--disable-examples-build --disable-static)
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc NEWS README RELEASE-NOTES
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
