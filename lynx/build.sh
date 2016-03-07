f6b34ec37564e2b9009f47ed06915c2aa0479c52
Tom G. Christensen <tgc@jupiterrise.com>
2016-03-07 17:14:36 +0100

    lynx: new package, lynx 2.8.9dev.8

---
+++ lynx/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=lynx
+version=2.8.9dev.8
+pkgver=1
+source[0]=http://invisible-mirror.net/archives/$topdir/tarballs/${topdir}${version}.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+topsrcdir=${topdir}${version}
+configure_args+=(--with-ssl)
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    ${__gsed} -i 's|^#! /bin/sh|#!/bin/bash|' configure
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYHEADER COPYING CHANGES README AUTHORS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
