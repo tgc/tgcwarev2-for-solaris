37d3e44a146f3640663c1e4bd9792d1ccc7704a7
Tom G. Christensen <tgc@jupiterrise.com>
2016-03-11 14:49:47 +0100

    openssh: update to 7.2p2

--- openssh/build.sh
+++ openssh/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssh
-version=7.2p1
+version=7.2p2
 pkgver=1
 source[0]=ftp://ftp.heanet.ie/pub/OpenBSD/OpenSSH/portable/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
