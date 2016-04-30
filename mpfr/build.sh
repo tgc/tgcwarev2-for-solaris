a19fa70588af73cdd0c39b308def7ae6ac7c2dfe
Tom G. Christensen <tgc@jupiterrise.com>
2016-04-30 11:32:02 +0200

    mpfr: update to 3.1.4

--- mpfr/build.sh
+++ mpfr/build.sh
@@ -6,12 +6,12 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=mpfr
-real_version=3.1.2
-version=${real_version}p11
+real_version=3.1.4
+version=${real_version}
 pkgver=1
-source[0]=http://www.mpfr.org/mpfr-current/$topdir-$real_version.tar.bz2
+source[0]=http://www.mpfr.org/mpfr-current/$topdir-$real_version.tar.xz
 # If there are no patches, simply comment this
-patch[0]=mpfr-3.1.2-allpatches-p1-p11.patch
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -47,6 +47,7 @@ install()
     ${__mv} ${stagedir}${prefix}/share/doc/mpfr ${stagedir}${prefix}/${_docdir}/mpfr-$version
     compat mpfr 3.1.1 1 1
     compat mpfr 3.1.2 1 1
+    compat mpfr 3.1.2p11 1 1
 }
 
 reg pack
