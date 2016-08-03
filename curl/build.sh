fb05da97bbe99c54f6c6bc9edfbfe5cc7f7c75fd
Tom G. Christensen <tgc@jupiterrise.com>
2016-08-04 00:12:02 +0200

    curl: update to 7.50

--- curl/build.sh
+++ curl/build.sh
@@ -6,9 +6,12 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=7.49.0
+version=7.50.0
 pkgver=1
 source[0]=http://curl.haxx.se/download/$topdir-$version.tar.bz2
+# https://curl.haxx.se/docs/caextract.html
+certdate=2016-04-20
+source[1]=cacert-$certdate.pem
 # If there are no patches, simply comment this
 #patch[0]=
 
@@ -19,7 +22,7 @@ source[0]=http://curl.haxx.se/download/$topdir-$version.tar.bz2
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 
-configure_args+=(--enable-static=no --enable-http --enable-ftp --enable-file --disable-ldap --enable-manual --enable-cookies --enable-crypto --with-libidn --with-libssh2)
+configure_args+=(--enable-static=no --enable-http --enable-ftp --enable-file --disable-ldap --enable-manual --enable-cookies --enable-crypto --with-libidn --with-libssh2 --with-ca-bundle=${prefix}/${_sysconfdir}/curl-ca-bundle.pem)
 
 reg prep
 prep()
@@ -43,6 +46,7 @@ reg install
 install()
 {
     generic_install DESTDIR
+    ${__install} -m0644 -D $(get_source_absfilename "${source[1]}") ${stagedir}${prefix}/${_sysconfdir}/curl-ca-bundle.pem
     doc CHANGES COPYING README* RELEASE-NOTES docs/FAQ docs/FEATURES docs/BUGS \
       docs/MANUAL docs/RESOURCES docs/TODO docs/TheArtOfHttpScripting \
       docs/examples/*.c docs/examples/Makefile.example docs/INTERNALS \
@@ -61,6 +65,8 @@ install()
     compat curl 7.46.0 1 1
     compat curl 7.47.1 1 2
     compat curl 7.48.0 1 1
+    compat curl 7.49.0 1 1
+    compat curl 7.49.1 1 1
 }
 
 reg pack
