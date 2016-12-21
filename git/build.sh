06f04af10a32e1891a6f6c87bd578388a17a979a
Tom G. Christensen <tgc@jupiterrise.com>
2016-12-21 20:41:44 +0100

    git: update to 2.11.0

--- git/build.sh
+++ git/build.sh
@@ -6,13 +6,13 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=git
-version=2.8.3
+version=2.11.0
 pkgver=1
 source[0]=https://www.kernel.org/pub/software/scm/git/$topdir-$version.tar.gz
 source[1]=https://www.kernel.org/pub/software/scm/git/$topdir-manpages-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=git-1.8.1.5-inet_addrstrlen.patch
-patch[1]=git-2.6.3-stdint_h.patch
+patch[1]=git-2.11.0-stdint_h.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -34,7 +34,9 @@ prep()
     setdir source
     cat << EOF > config.mak
 CC=gcc
+SHELL=/usr/tgcware/bin/bash
 PERL_PATH=$prefix/bin/perl
+SHELL_PATH=$prefix/bin/bash
 ICONVDIR=$prefix
 SANE_TOOL_PATH=/usr/tgcware/gnu:/usr/xpg6/bin:/usr/xpg4/bin
 NO_INSTALL_HARDLINKS=YesPlease
