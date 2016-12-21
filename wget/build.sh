59a47f1d1f0ab191ccbe6487f64ded670e47684d
Tom G. Christensen <tgc@jupiterrise.com>
2016-12-21 22:50:16 +0100

    wget: rebuild

--- wget/build.sh
+++ wget/build.sh
@@ -7,7 +7,7 @@
 # Check the following 4 variables before running the script
 topdir=wget
 version=1.18
-pkgver=1
+pkgver=2
 source[0]=https://mirrors.kernel.org/gnu/wget/$topdir-$version.tar.xz
 # If there are no patches, simply comment this
 #patch[0]=
