542039e24b2c95472064e026b150caf77686a464
Tom G. Christensen <tgc@jupiterrise.com>
2016-12-21 21:05:34 +0100

    links: update to 2.14

--- links/build.sh
+++ links/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=links
-version=2.12
+version=2.14
 pkgver=1
 source[0]=http://links.twibright.com/download/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
