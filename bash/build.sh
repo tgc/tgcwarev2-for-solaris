4ca7e8cbdacf22fc8ecacc330157eef63122efd3
Tom G. Christensen <tgc@jupiterrise.com>
2017-09-30 12:54:25 +0200

    bash: update to 4.4.12

--- bash/build.sh
+++ bash/build.sh
@@ -6,57 +6,27 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=bash
-real_version=4.3
-version=4.3.42
+real_version=4.4
+version=4.4.12
 pkgver=1
-source[0]=ftp://ftp.sunet.se/pub/gnu/bash/$topdir-$real_version.tar.gz
+source[0]=http://mirrors.kernel.org/gnu/bash/$topdir-$real_version.tar.gz
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
-patch[0]=""   # no sparse array support in buildpkg
-patch[1]=bash43-001
-patch[2]=bash43-002
-patch[3]=bash43-003
-patch[4]=bash43-004
-patch[5]=bash43-005
-patch[6]=bash43-006
-patch[7]=bash43-007
-patch[8]=bash43-008
-patch[9]=bash43-009
-patch[10]=bash43-010
-patch[11]=bash43-011
-patch[12]=bash43-012
-patch[13]=bash43-013
-patch[14]=bash43-014
-patch[15]=bash43-015
-patch[16]=bash43-016
-patch[17]=bash43-017
-patch[18]=bash43-018
-patch[19]=bash43-019
-patch[20]=bash43-020
-patch[21]=bash43-021
-patch[22]=bash43-022
-patch[23]=bash43-023
-patch[24]=bash43-024
-patch[25]=bash43-025
-patch[26]=bash43-026
-patch[27]=bash43-027
-patch[28]=bash43-028
-patch[29]=bash43-029
-patch[30]=bash43-030
-patch[31]=bash43-031
-patch[32]=bash43-032
-patch[33]=bash43-033
-patch[34]=bash43-034
-patch[35]=bash43-035
-patch[36]=bash43-036
-patch[37]=bash43-037
-patch[38]=bash43-038
-patch[39]=bash43-039
-patch[40]=bash43-040
-patch[41]=bash43-041
-patch[42]=bash43-042
+patch[0]=bash-4.4-no-loadable-builtins.patch
+patch[1]=bash44-001
+patch[2]=bash44-002
+patch[3]=bash44-003
+patch[4]=bash44-004
+patch[5]=bash44-005
+patch[6]=bash44-006
+patch[7]=bash44-007
+patch[8]=bash44-008
+patch[9]=bash44-009
+patch[10]=bash44-010
+patch[11]=bash44-011
+patch[12]=bash44-012
 
 # Global settings
 export CPPFLAGS="-I$prefix/include"
@@ -99,6 +69,7 @@ install()
     compat bash 4.3.30 1 1
     compat bash 4.3.33 1 1
     compat bash 4.3.39 1 1
+    compat bash 4.3.42 1 1
 }
 
 reg pack
