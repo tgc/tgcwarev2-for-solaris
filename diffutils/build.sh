f5ee9ae4558443c7f346e7104e93c2ec5c8fb9bc
Tom G. Christensen <tgc@jupiterrise.com>
2019-03-24 17:06:09 +0100

    diffutils: update to 3.7

--- diffutils/build.sh
+++ diffutils/build.sh
@@ -6,11 +6,11 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=diffutils
-version=3.3
-pkgver=2
-source[0]=ftp://ftp.sunet.se/pub/gnu/diffutils/$topdir-$version.tar.xz
+version=3.7
+pkgver=1
+source[0]=https://mirrors.kernel.org/gnu/diffutils/$topdir-$version.tar.xz
 # If there are no patches, simply comment this
-patch[0]=diffutils-3.3-fix-locale-install.patch
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -42,7 +42,6 @@ reg install
 install()
 {
     generic_install DESTDIR
-    rmdir ${stagedir}${prefix}/${_libdir}
     doc NEWS THANKS AUTHORS COPYING
 }
 
