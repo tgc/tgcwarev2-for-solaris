82e89f0bd471a17055db53475f97333239d99b56
Tom G. Christensen <tgc@jupiterrise.com>
2019-03-24 20:00:14 +0100

    bash: update to 4.4.23

--- bash/build.sh
+++ bash/build.sh
@@ -7,7 +7,7 @@
 # Check the following 4 variables before running the script
 topdir=bash
 real_version=4.4
-version=4.4.12
+version=4.4.23
 pkgver=1
 source[0]=http://mirrors.kernel.org/gnu/bash/$topdir-$real_version.tar.gz
 
@@ -27,6 +27,17 @@ patch[9]=bash44-009
 patch[10]=bash44-010
 patch[11]=bash44-011
 patch[12]=bash44-012
+patch[13]=bash44-013
+patch[14]=bash44-014
+patch[15]=bash44-015
+patch[16]=bash44-016
+patch[17]=bash44-017
+patch[18]=bash44-018
+patch[19]=bash44-019
+patch[20]=bash44-020
+patch[21]=bash44-021
+patch[22]=bash44-022
+patch[23]=bash44-023
 
 # Global settings
 export CPPFLAGS="-I$prefix/include"
@@ -70,6 +81,7 @@ install()
     compat bash 4.3.33 1 1
     compat bash 4.3.39 1 1
     compat bash 4.3.42 1 1
+    compat bash 4.4.12 1 1
 }
 
 reg pack
