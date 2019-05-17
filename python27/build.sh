0516666ffc5777c8e89a8dc1a9a7202349cfa4b7
Tom G. Christensen <tgc@jupiterrise.com>
2019-05-17 11:52:41 +0200

    python27: update to 2.7.16

--- python27/build.sh
+++ python27/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=python
-version=2.7.15
+version=2.7.16
 pkgver=1
 source[0]=https://www.python.org/ftp/python/$version/Python-$version.tar.xz
 # If there are no patches, simply comment this
@@ -78,7 +78,7 @@ install()
     docs_for python27 LICENSE README
     docs_for python27-libs LICENSE README
 
-    for ver in 2.7.9 2.7.10 2.7.11 2.7.12 2.7.13
+    for ver in 2.7.9 2.7.10 2.7.11 2.7.12 2.7.13 2.7.14 2.7.15 2.7.16
     do
         compat python27 $ver 1 1
         compat python27-libs $ver 1 1
