6a0a3dae20c2a3bb1ccb5d2bded96d411667cc19
Tom G. Christensen <tgc@jupiterrise.com>
2019-05-26 13:42:56 +0200

    binutils: update to 2.32

--- binutils/build.sh
+++ binutils/build.sh
@@ -6,11 +6,11 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=binutils
-version=2.25
+version=2.32
 pkgver=1
-source[0]=ftp://ftp.sunet.se/pub/gnu/binutils/$topdir-$version.tar.bz2
+source[0]=https://ftpmirror.gnu.org/gnu/binutils/$topdir-$version.tar.lz
 # If there are no patches, simply comment this
-patch[0]=binutils-2.25-use-strtod-instead-of-strtold.patch
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -19,6 +19,8 @@ patch[0]=binutils-2.25-use-strtod-instead-of-strtold.patch
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 configure_args+=(--disable-werror --program-prefix=g)
+# gcc dies with an ICE when building ld on Solaris 7
+[ "$gnu_os_ver" = "2.7" ] && configure_args+=(--enable-ld=no)
 
 reg prep
 prep()
@@ -46,6 +48,7 @@ install()
     doc COPYING*
     compat binutils 2.22 1 6
     compat binutils 2.23.1 1 1
+    compat binutils 2.25 1 1
 }
 
 reg pack
