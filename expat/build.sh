74b4b3a29606192119dbdc30d227866f8066ae03
Tom G. Christensen <tgc@jupiterrise.com>
2020-03-14 20:10:04 +0100

    expat: update to 2.2.9

--- expat/build.sh
+++ expat/build.sh
@@ -6,9 +6,9 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=expat
-version=2.2.5
+version=2.2.9
 pkgver=1
-source[0]=http://downloads.sourceforge.net/project/expat/expat/${version}/$topdir-$version.tar.bz2
+source[0]=https://downloads.sourceforge.net/project/expat/expat/${version}/$topdir-$version.tar.lz
 # If there are no patches, simply comment this
 #patch[0]=
 
@@ -43,9 +43,10 @@ install()
 {
     generic_install DESTDIR
     doc COPYING Changes README.md AUTHORS
-    compat expat 2.1.0 1 1
     ${__rm} -rf ${stagedir}${prefix}/share/doc/expat
 
+    compat expat 2.1.0 1 1
+    compat expat 2.2.5 1 1
 }
 
 reg pack
