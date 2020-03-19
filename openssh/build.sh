831644a6e3fbc7fddec707d9d0537599a58cfd02
Tom G. Christensen <tgc@jupiterrise.com>
2020-03-19 20:14:31 +0100

    openssh: update to 8.2p1

--- openssh/build.sh
+++ openssh/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssh
-version=8.1p1
+version=8.2p1
 pkgver=1
 source[0]=https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
