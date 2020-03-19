5d1acec279dd4f8f8603551b0e7eccaffc4a503f
Tom G. Christensen <tgc@jupiterrise.com>
2020-03-19 18:12:03 +0100

    openssl: update to 1.0.2u

--- openssl/build.sh
+++ openssl/build.sh
@@ -6,9 +6,9 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssl
-version=1.0.2r
-pkgver=5
-source[0]=http://openssl.org/source/$topdir-$version.tar.gz
+version=1.0.2u
+pkgver=6
+source[0]=https://openssl.org/source/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
 
@@ -27,7 +27,8 @@ configure_args=(--prefix=$prefix --openssldir=${prefix}/${_sharedir}/ssl zlib-dy
 if [ "$arch" = "sparc" ]; then
     configure_args+=(solaris-sparc${gcc_arch}-gcc)
 else
-    configure_args+=(386 solaris-x86-gcc)
+    # SSE2 requires Solaris 9 4/04 or later
+    configure_args+=(solaris-x86-gcc no-sse2)
 fi
 
 # Buildsystem is non-standard so we take the easy way out
@@ -37,29 +38,32 @@ reg prep
 prep()
 {
     generic_prep
-}
 
-reg build
-build()
-{
     setdir source
     ${__gsed} -i '/^SHELL/s/sh/ksh/' Makefile.org
     ${__gsed} -i "s;@LIBDIR@;${prefix}/lib;g" Makefile.org
 
     if [ "$arch" = "i386" ]; then
-	# openssl defaults to --march=pentium which should be changed
+	# Override the default gcc march
 	${__gsed} -i "/solaris-x86-gcc/ s;-march=pentium;-march=$gcc_arch;" Configure
-	# There is no reason to disable inline asm
+	# With GNU as there is no need to disable inline assembler
 	${__gsed} -i "/solaris-x86-gcc/ s; -DOPENSSL_NO_INLINE_ASM;;" Configure
     fi
     # The -mv8 alias is not supported with newer gcc
     ${__gsed} -i 's/mv8/mcpu=v8/g' Configure
 
+    ${__gsed} -i "/^CFLAG=/s;CFLAG=;CFLAG=-I${prefix}/include;" Makefile
+    ${__gsed} -i "/EX_LIBS/s;-lz;-L${prefix}/lib -R${prefix}/lib -lz;" Makefile
+}
+
+reg build
+build()
+{
     echo $__configure "${configure_args[@]}"
     $__configure "${configure_args[@]}"
 
-    ${__gsed} -i "/^CFLAG=/s;CFLAG=;CFLAG=-I${prefix}/include;" Makefile
-    ${__gsed} -i "/EX_LIBS/s;-lz;-L${prefix}/lib -R${prefix}/lib -lz;" Makefile
+    setdir source
+
     ${__make} SHARED_LDFLAGS="-shared -R${prefix}/${_libdir}" depend
     ${__make} SHARED_LDFLAGS="-shared -R${prefix}/${_libdir}"
 }
@@ -108,6 +112,7 @@ install()
     compat openssl 1.0.2k 1 2
     compat openssl 1.0.2o 1 3
     compat openssl 1.0.2p 1 4
+    compat openssl 1.0.2r 1 5
 }
 
 reg pack
