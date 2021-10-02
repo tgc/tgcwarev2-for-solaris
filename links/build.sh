3f1bf16b9fb26a474fe0228f46c86c2254e33059
Tom G. Christensen <tgc@jupiterrise.com>
2021-10-02 17:20:02 +0200

    links: update to 2.24

--- links/build.sh
+++ links/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=links
-version=2.20.2
+version=2.24
 pkgver=1
 source[0]=https://links.twibright.com/download/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
