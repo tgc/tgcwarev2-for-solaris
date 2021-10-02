155fc7817d614430cef43ae03bcce6ddc7cda1f9
Tom G. Christensen <tgc@jupiterrise.com>
2021-10-02 16:33:17 +0200

    libidn2: update to 2.3.2

--- libidn2/build.sh
+++ libidn2/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=libidn2
-version=2.3.0
+version=2.3.2
 pkgver=1
 source[0]=https://www.mirrorservice.org/sites/ftp.gnu.org/gnu/libidn/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -53,6 +53,7 @@ install()
     compat libidn2 2.0.3 1 1
     compat libidn2 2.0.4 1 1
     compat libidn2 2.1.1a 1 1
+    compat libidn2 2.3.0 1 1
 }
 
 reg pack
