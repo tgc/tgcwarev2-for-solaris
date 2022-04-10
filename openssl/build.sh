72dc0d8fec393d8a642196ccb370c18396712a75
Tom G. Christensen <tgc@jupiterrise.com>
2022-04-10 20:10:04 +0200

    openssl: add security patches
    Fixes all relevant CVEs with patches from RHEL 7.
    
    Additionally tweak for parallel install installation with OpenSSL 3.x.

--- openssl/build.sh
+++ openssl/build.sh
@@ -7,10 +7,14 @@
 # Check the following 4 variables before running the script
 topdir=openssl
 version=1.0.2u
-pkgver=6
+pkgver=8
 source[0]=https://openssl.org/source/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=openssl-1.0.2u-cve-2020-1971.patch
+patch[1]=openssl-1.0.2u-cve-2021-23840.patch
+patch[2]=openssl-1.0.2u-cve-2021-23841.patch
+patch[3]=openssl-1.0.2u-cve-2021-3712.patch
+patch[4]=openssl-1.0.2u-cve-2022-0778.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -19,11 +23,12 @@ source[0]=https://openssl.org/source/$topdir-$version.tar.gz
 . ${BUILDPKG_BASE}/gcc/build.sh.gcc.cpu
 
 # Global settings
-abbrev_ver=$(echo $version|${__sed} -e 's/\.//g')
-baseversion=$(echo $version|${__sed} -e 's/[a-zA-Z]//g')
+sover=1.0.0
+shortver=102
+pname=openssl${shortver}
 make_check_target="test"
 __configure="./Configure"
-configure_args=(--prefix=$prefix --openssldir=${prefix}/${_sharedir}/ssl zlib-dynamic shared)
+configure_args=(--prefix=$prefix --openssldir=${prefix}/${_sharedir}/ssl${shortver} zlib shared)
 if [ "$arch" = "sparc" ]; then
     configure_args+=(solaris-sparc${gcc_arch}-gcc)
 else
@@ -59,10 +64,10 @@ prep()
 reg build
 build()
 {
+    setdir source
+
     echo $__configure "${configure_args[@]}"
     $__configure "${configure_args[@]}"
-
-    setdir source
 
     ${__make} SHARED_LDFLAGS="-shared -R${prefix}/${_libdir}" depend
     ${__make} SHARED_LDFLAGS="-shared -R${prefix}/${_libdir}"
@@ -80,21 +85,35 @@ install()
     clean stage
     setdir source
     ${__make} INSTALL_PREFIX=$stagedir MANDIR=${prefix}/${_mandir} install
-    setdir ${stagedir}${prefix}/${_mandir}
-    for j in $(${__ls} -1d man?)
+    # Relocate
+    mkdir -p ${stagedir}${prefix}/{${_includedir},${_libdir}}/$pname
+    ${__mv} ${stagedir}${prefix}/${_includedir}/{openssl,$pname/openssl}
+    ${__mv} ${stagedir}${prefix}/${_bindir}/{openssl,$pname}
+    ${__rm} -rf ${stagedir}${prefix}/{${_sharedir}/ssl/misc,{${_bindir},${_mandir}/man1}/{CA.pl,c_rehash,*tsget}*}
+    ${__rm} -f ${stagedir}${prefix}/${_libdir}/*.so
+    ln -s ../libcrypto.so.${sover} ${stagedir}${prefix}/${_libdir}/${pname}/libcrypto.so
+    ln -s ../libssl.so.${sover} ${stagedir}${prefix}/${_libdir}/${pname}/libssl.so
+    for pc in libcrypto libssl openssl
     do
-	cd $j
-	for manpage in *
-	do
-	    if [ -L "${manpage}" ]; then
-                TARGET=$(${__ls} -l "${manpage}" | ${__awk} '{ print $NF }')
-                ${__ln} -snf "${TARGET}"ssl "${manpage}"ssl
-                ${__rm} -f "${manpage}"
-	    else
-		${__mv} "$manpage" "$manpage""ssl"
-	    fi
-	done
-	cd ..
+	${__sed} -e "s@\(Libs: -L\${libdir}\)@\1 -L\${libdir}/$pname@" \
+	         -e "s@\(Cflags: -I\${includedir}\)@\1 -I\${includedir}/$pname@" \
+		 -e "s@\(Requires.*:.*\)\(libssl\)@\1\2$shortver@g" \
+		 -e "s@\(Requires.*:.*\)\(libcrypto\)@\1\2$shortver@g" \
+		 ${stagedir}${prefix}/${_libdir}/pkgconfig/${pc}.pc > ${stagedir}${prefix}/${_libdir}/pkgconfig/${pc}${shortver}.pc
+	rm -f ${stagedir}${prefix}/${_libdir}/pkgconfig/${pc}.pc
+    done
+    setdir ${stagedir}${prefix}/${_mandir}
+    ${__mv} man1/{openssl.1,${pname}.1}
+    for manpage in man*/*
+    do
+	[ "${manpage}" = "man1/${pname}.1" ] && continue
+	if [ -L "${manpage}" ]; then
+	    TARGET=$(${__ls} -l ${manpage} | ${__awk} '{ print $NF }')
+	    ${__ln} -snf ${TARGET}ssl${shortver} ${manpage}ssl${shortver}
+	    ${__rm} -f ${manpage}
+	else
+	    ${__mv} $manpage ${manpage}ssl${shortver}
+	fi
     done
     # Make .sos writable
     chmod 755 ${stagedir}${prefix}/${_libdir}/*.so.*
@@ -113,6 +132,8 @@ install()
     compat openssl 1.0.2o 1 3
     compat openssl 1.0.2p 1 4
     compat openssl 1.0.2r 1 5
+    compat openssl 1.0.2u 1 6
+    compat openssl 1.0.2u 1 7
 }
 
 reg pack
