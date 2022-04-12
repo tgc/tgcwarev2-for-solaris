142fa54d890c279097ca1d3b3b179ead7ad43ec5
Tom G. Christensen <tgc@jupiterrise.com>
2022-04-12 20:30:26 +0200

    nghttp2: update to 1.47.0

--- nghttp2/build.sh
+++ nghttp2/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=nghttp2
-version=1.45.1
+version=1.47.0
 pkgver=1
 source[0]=https://github.com/${topdir}/${topdir}/releases/download/v${version}/${topdir}-${version}.tar.xz
 # If there are no patches, simply comment this
@@ -61,6 +61,7 @@ install()
     compat nghttp2 1.38.0 1 1
     compat nghttp2 1.40.0 1 1
     compat nghttp2 1.41.0 1 1
+    compat nghttp2 1.45.1 1 1
 }
 
 reg pack
