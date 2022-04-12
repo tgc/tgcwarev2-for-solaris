a05365ae8a37dadeb4397ee6e631e21cc9f13838
Tom G. Christensen <tgc@jupiterrise.com>
2022-04-12 21:39:42 +0200

    git: rebuild with OpenSSL 3.0

--- git/build.sh
+++ git/build.sh
@@ -7,7 +7,7 @@
 # Check the following 4 variables before running the script
 topdir=git
 version=2.16.6
-pkgver=1
+pkgver=2
 source[0]=https://www.kernel.org/pub/software/scm/git/$topdir-$version.tar.gz
 source[1]=https://www.kernel.org/pub/software/scm/git/$topdir-manpages-$version.tar.gz
 # If there are no patches, simply comment this
