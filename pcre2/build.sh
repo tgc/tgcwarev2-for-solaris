09ee0b639682fc87ac8a70ec9b59db0904385140
Tom G. Christensen <tgc@jupiterrise.com>
2022-06-10 17:05:22 +0200

    pcre2: new package, pcre2 10.40

---
+++ pcre2/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=pcre2
+version=10.40
+pkgver=1
+source[0]=https://github.com/PCRE2Project/$topdir/releases/download/$topdir-$version/$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+configure_args+=(--disable-percent-zt)
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
