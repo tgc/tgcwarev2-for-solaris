e6b479866c69d1fdcbd7afe6724235c8ad0bd438
Tom G. Christensen <tgc@jupiterrise.com>
2022-06-10 21:36:19 +0200

    git: update to 2.36.1

--- git/build.sh
+++ git/build.sh
@@ -6,44 +6,41 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=git
-version=2.16.6
-pkgver=2
+version=2.36.1
+pkgver=1
 source[0]=https://www.kernel.org/pub/software/scm/git/$topdir-$version.tar.gz
 source[1]=https://www.kernel.org/pub/software/scm/git/$topdir-manpages-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=git-1.8.1.5-inet_addrstrlen.patch
-patch[1]=git-2.11.0-stdint_h.patch
-patch[2]=git-2.14.1-fix-t5545.patch
+patch[0]=0001-Ensure-INET_ADDRSTRLEN-is-defined.patch
+patch[1]=0002-Ensure-SCNuMAX-is-defined.patch
+patch[2]=0003-Update-common-Solaris-settings.patch
+patch[3]=0004-Use-largefile-environment-on-Solaris.patch
+patch[4]=0005-Update-settings-for-Solaris.patch
+patch[5]=0006-No-pthread-support-for-Solaris-2.6.patch
+patch[6]=0007-Solaris-2.6-needs-libresolv.patch
+patch[7]=0008-Use-better-shell-in-t5545.patch
+patch[8]=0009-Use-better-shell-in-t5801-helper.patch
+patch[9]=0010-Avoid-stdint.h.patch
+patch[10]=0011-Workaround-for-fileno-being-a-macro.patch
+[ $(uname -r) = "5.7" ] && patch[11]=0012-UTC-is-GMT-on-Solaris-8.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
+# Consistently failing tests
+# Solaris <= 9
+#t3701.53 - diffFilter filters diff
+# Solaris <= 8
+#t8010.9 - cat-file --textconv --batch works
+# cat-file --textconv is buggy. As soon as the textconv command is triggered on
+# an input line all subsequent input lines are dropped from the output.
+
 # Global settings
-nosv=$(printf "5%02d" ${gnu_os_ver##*.})
-if [ $nosv -ge 507 ]; then # Solaris >= 7
-    # fails to distinguish file vs. dir
-    GIT_SKIP_TESTS="t1308.23"
-fi
-if [ $nosv -lt 509 ]; then # Solaris < 9
-    # cat-file --textconv is buggy, output is truncated
-    GIT_SKIP_TESTS="$GIT_SKIP_TESTS t8010.8 t8010.9"
-fi
-if [ $nosv -eq 507 ]; then # Solaris = 7
-    # SIGPIPE issues
-    GIT_SKIP_TESTS="$GIT_SKIP_TESTS t0005.4 t0005.5"
-    GIT_SKIP_TESTS="$GIT_SKIP_TESTS t0021.6 t0021.9"
-    GIT_SKIP_TESTS="$GIT_SKIP_TESTS t5571.11"
-    GIT_SKIP_TESTS="$GIT_SKIP_TESTS t5801.24 t5801.25 t5801.27"
-    # TZ=UTC returns GMT instead of UTC
-    GIT_SKIP_TESTS="$GIT_SKIP_TESTS t0006.25"
-    # fatal: cannot lock ref 'HEAD': Unable to create 'git-2.14.2/t/trash directory.t7600-merge/.git/HEAD.lock': File exists.
-    GIT_SKIP_TESTS="$GIT_SKIP_TESTS t7600.56"
-fi
-export GIT_SKIP_TESTS
 no_configure=1
 __configure="make"
 configure_args=
-make_check_target="test"
+make_check_target="test V=1"
+make_build_target="V=1"
 
 reg prep
 prep()
@@ -52,22 +49,34 @@ prep()
     setdir source
     cat << EOF > config.mak
 V=1
+prefix=$prefix
 CC=gcc
-SHELL=/usr/tgcware/bin/bash
+SHELL=$prefix/bin/bash
 PERL_PATH=$prefix/bin/perl
 SHELL_PATH=$prefix/bin/bash
-ICONVDIR=$prefix
 SANE_TOOL_PATH=/usr/tgcware/gnu:/usr/xpg6/bin:/usr/xpg4/bin
+BASIC_CFLAGS+=-std=gnu99
+BASIC_CFLAGS+=-I$prefix/include
+BASIC_LDFLAGS+=-L$prefix/lib -R$prefix/lib
+PTHREAD_CFLAGS=-pthread
+INSTALL=/usr/tgcware/bin/ginstall
+TAR=/usr/tgcware/bin/gtar
+USE_LIBPCRE=YesPlease
+NEEDS_LIBICONV=YesPlease
+ICONVDIR=$prefix
 NO_INSTALL_HARDLINKS=YesPlease
-BASIC_CFLAGS += -I/usr/tgcware/include
-BASIC_LDFLAGS += -L/usr/tgcware/lib -Wl,-R,/usr/tgcware/lib
-NEEDS_LIBICONV = YesPlease
-NO_PYTHON = YesPlease
-INSTALL = /usr/tgcware/bin/ginstall
-TAR = /usr/tgcware/bin/gtar
-prefix=$prefix
+NO_PYTHON=YesPlease
 # It takes forever to run SVN tests
-NO_SVN_TESTS = YesPlease
+NO_SVN_TESTS=YesPlease
+# Bypass curl-config
+CURLDIR=$prefix
+CURL_LDFLAGS=-lcurl
+# This is a safe choice for Solaris < 9 where /dev/urandom requires either a
+# patch or a third party kernel driver like ANDIrand
+CSPRNG_METHOD=openssl
+# Test options
+DEFAULT_TEST_TARGET=prove
+GIT_PROVE_OPTS=--timer --jobs 4 --state=save --statefile=$srcdir/$topsrcdir/.prove
 EOF
 
 }
