472e22a114bac06e2ce0f9de062f22f9747ee4d5
Tom G. Christensen <tgc@jupiterrise.com>
2022-11-04 19:38:49 +0100

    git: update to 2.36.3

--- git/build.sh
+++ git/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=git
-version=2.36.1
+version=2.36.3
 pkgver=1
 source[0]=https://www.kernel.org/pub/software/scm/git/$topdir-$version.tar.gz
 source[1]=https://www.kernel.org/pub/software/scm/git/$topdir-manpages-$version.tar.gz
