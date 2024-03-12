3c1759a21c469ef6bd170532dbc59e722347c98b
Tom G. Christensen <tgc@jupiterrise.com>
2024-03-12 18:13:48 +0100

    openssl3: update to 3.0.13

--- openssl3/build.sh
+++ openssl3/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssl
-version=3.0.12
+version=3.0.13
 pkgver=1
 source[0]=https://openssl.org/source/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -91,6 +91,7 @@ install()
     compat openssl 3.0.9 1 1
     compat openssl 3.0.10 1 1
     compat openssl 3.0.11 1 1
+    compat openssl 3.0.12 1 1
 }
 
 reg pack
