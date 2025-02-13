a826a7ce4e267bf66aa31cfc25d50bf391e65bf5
Tom G. Christensen <tgc@jupiterrise.com>
2025-02-13 14:21:36 +0100

    rsync: update to 3.4.1

--- rsync/build.sh
+++ rsync/build.sh
@@ -6,11 +6,14 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=rsync
-version=3.1.3
+version=3.4.1
 pkgver=1
-source[0]=http://rsync.samba.org/ftp/rsync/src/$topdir-$version.tar.gz
+source[0]=https://download.samba.org/pub/rsync/src/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=0001-Move-pathjoin-to-lib.patch
+patch[1]=0002-Fix-build-with-no-stdint.h.patch
+patch[2]=0003-Ensure-SIZE_MAX-is-defined.patch
+patch[3]=0004-Missing-include-for-my_strdup.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -18,17 +21,12 @@ source[0]=http://rsync.samba.org/ftp/rsync/src/$topdir-$version.tar.gz
 # Global settings
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
-configure_args+=(--with-included-popt)
+configure_args+=(--with-included-popt --disable-xxhash --disable-zstd --disable-lz4)
 
 reg prep
 prep()
 {
     generic_prep
-    setdir source
-    # Fix IPv6 test
-    # It should test for something that the header would define instead of relying
-    # on the preprocesser to stop when the include fails (gcc < 4.5 does not)
-    ${__gsed} -i 's/__sun/_NETINET_IP6_H/g' configure.sh
 }
 
 reg build
@@ -47,7 +45,7 @@ reg install
 install()
 {
     generic_install DESTDIR
-    doc NEWS README COPYING
+    doc NEWS.md README.md COPYING
 }
 
 reg pack
